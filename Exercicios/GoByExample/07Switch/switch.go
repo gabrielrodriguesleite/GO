b98c55341efa5713b2937c77a6966eec3a717f85
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-09 19:46:10 -0300

    [feat] go by examples exercicio 07 switch basico

---
+++ Exercicios/GoByExample/07Switch/switch.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// basico
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+}
