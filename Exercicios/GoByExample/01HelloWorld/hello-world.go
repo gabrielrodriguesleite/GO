07c595a0751611065eb2b5479762e4f55baecfb0
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-09 17:51:31 -0300

    [feat] go by example 01 hello world

---
+++ Exercicios/GoByExample/01HelloWorld/hello-world.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
+
+// é sempre importante abrir um workspace (no vscode) na pasta raiz do modulo
+
+// Para rodar:
+// go run hello-world.go
+
+// Para buildar:
+// go build hello-world.go
+
+// Para rodar o build:
+// ./hello-world
