1b925de227cd8f638dad2195e55298e4416723bb
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-09 20:30:35 -0300

    [feat] go by examples exercicio 08 arrays, declarar uma composição de array (tipo multidimensional)

--- Exercicios/GoByExample/08Arrays/arrays.go
+++ Exercicios/GoByExample/08Arrays/arrays.go
@@ -20,4 +20,12 @@ func main() {
 	b := [5]int{1, 2, 3, 4, 5}
 	fmt.Println("dcl:", b)
 
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+
 }
