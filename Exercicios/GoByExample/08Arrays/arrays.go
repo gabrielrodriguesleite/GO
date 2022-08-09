f15e664d4a3184894082c33aba2a95e2403d4f95
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-09 20:25:09 -0300

    [doc] exemplo 08 arrays, set, get, len

--- Exercicios/GoByExample/08Arrays/arrays.go
+++ Exercicios/GoByExample/08Arrays/arrays.go
@@ -8,6 +8,8 @@ func main() {
 	var a [5]int
 	fmt.Println("emp:", a)
 
+	// usando um indice podemos definir um valor e ler um valor
+	// a função len() retorna o tamanho
 	a[4] = 100
 	fmt.Println("set:", a)
 	fmt.Println("get:", a[4])
