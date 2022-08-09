6e81581c9ebcb1ddc877541a1c8238b44cd048d7
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-09 20:23:41 -0300

    [feat] go by examples exercicio 08 arrays, set, get, len

--- Exercicios/GoByExample/08Arrays/arrays.go
+++ Exercicios/GoByExample/08Arrays/arrays.go
@@ -7,4 +7,10 @@ func main() {
 	// declaração de um array de inteiros, vazio com 5 posições
 	var a [5]int
 	fmt.Println("emp:", a)
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+
+	fmt.Println("len:", len(a))
 }
