9838325079ed780808b4f7108d4dcb31fd31ba02
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-09 19:41:18 -0300

    [feat] go by examples exercicio 06 exemplos de for comentarios

--- Exercicios/GoByExample/06IfElse/if-else.go
+++ Exercicios/GoByExample/06IfElse/if-else.go
@@ -4,16 +4,19 @@ import "fmt"
 
 func main() {
 
+	// basico if else
 	if 7%2 == 0 {
 		fmt.Println("7 is even")
 	} else {
 		fmt.Println("7 is odd")
 	}
 
+	// if sem else
 	if 8%4 == 0 {
 		fmt.Println("8 is divisible by 4")
 	}
 
+	// declaração pode preceder condição, a variável definida aqui está disponível em todos os braços
 	if num := 9; num < 0 {
 		fmt.Println(num, "is negative")
 	} else if num < 10 {
@@ -21,5 +24,9 @@ func main() {
 	} else {
 		fmt.Println(num, "ha multiple digits")
 	}
+
+	// parentesis não são obrigatórios mas chaves são
+
+	// não existe ternario então é preciso uma declaração completa inclusive para condições basicas
 
 }
