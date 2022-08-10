5eb6f7dd72cb2cb54da8c8e7bed07633f6ffea15
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-10 01:16:18 -0300

    [doc] go by example exercicio 11 range interando em map retornando chave e valor

--- Exercicios/GoByExample/11Range/range.go
+++ Exercicios/GoByExample/11Range/range.go
@@ -21,6 +21,7 @@ func main() {
 		}
 	}
 
+	// range iterando em map retornando chave e valor
 	kvs := map[string]string{"a": "apple", "b": "banana"}
 	for k, v := range kvs {
 		fmt.Printf("%s -> %s\n", k, v)
