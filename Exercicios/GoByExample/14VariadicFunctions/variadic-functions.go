059fd044800a0735463ae6466072bf07473d2070
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-10 08:58:04 -0300

    [doc] go by example exercicio 14 variadic functions

--- Exercicios/GoByExample/14VariadicFunctions/variadic-functions.go
+++ Exercicios/GoByExample/14VariadicFunctions/variadic-functions.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// função que recebe uma quantidade não definida de inteiros
+// nums recebe o tipo []int e terá o len com o número de items
 func sum(nums ...int) {
 
 	fmt.Print(nums, " ")
@@ -18,6 +20,8 @@ func main() {
 	sum(1, 2, 3)
 
 	nums := []int{1, 2, 3, 4}
+
+	// variadic functions podem receber um array ou slice espalhado
 	sum(nums...)
 
 }
