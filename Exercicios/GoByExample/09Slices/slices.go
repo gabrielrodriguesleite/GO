bcbb0ba0b0f19c85ad30b0eabb94ac6d606f9346
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-09 23:14:50 -0300

    [doc] go by example exercicio 09 slices

--- Exercicios/GoByExample/09Slices/slices.go
+++ Exercicios/GoByExample/09Slices/slices.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 
+	// slices são arrays com super poderes
+	// Para criar um slice vazio com tamanho diferente de zero se usa a palavra make
+	// aqui criamos um slice com 3 posições do tipo string
 	s := make([]string, 3)
 	fmt.Println("emp:", s)
 
