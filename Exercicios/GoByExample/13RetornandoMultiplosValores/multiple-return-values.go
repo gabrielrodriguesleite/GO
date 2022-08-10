5f0daf7e8faaf11a1b121d33115cc76c6f368682
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-10 08:46:09 -0300

    [feat] go by example exercicio 13 retorno de multiplos valores e multiplas atribuições

--- Exercicios/GoByExample/13RetornandoMultiplosValores/multiple-return-values.go
+++ Exercicios/GoByExample/13RetornandoMultiplosValores/multiple-return-values.go
@@ -3,15 +3,21 @@ package main
 import "fmt"
 
 // o recurso de retornar multiplos valores é suportado por Go
+// a assinatura da função mostra (int, int) indicando o retorno de 2 ints
 func vals() (int, int) {
 	return 3, 7
 }
 
 func main() {
 
+	// multipla atribuição de multiplos retornos
 	a, b := vals()
 	fmt.Println(a)
 	fmt.Println(b)
+
+	// retornos podem ser omitidos usando o _ blank identifier
+	_, c := vals()
+	fmt.Println(c)
 
 }
 
