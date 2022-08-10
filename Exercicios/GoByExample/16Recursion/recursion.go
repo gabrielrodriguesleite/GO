f21d4126c059ebabc259e0c74c8727dc9880b672
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-10 15:15:00 -0300

    [fix] nome corrigido

--- Exercicios/GoByExample/16Recursion/recursion.go
+++ Exercicios/GoByExample/16Recursion/recursion.go
@@ -14,4 +14,14 @@ func main() {
 
 	fmt.Println(fact(7)) // 5040
 
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+		return fib(n-1) + fib(n-2)
+	}
+
+	fmt.Println(fib(7))
 }
