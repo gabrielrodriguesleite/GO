23a22a4a2527a5c53f2386d8730e24c8addd25c6
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-10 01:11:40 -0300

    [doc] go by example exercicio 11 range interando em slice para somar itens

--- Exercicios/GoByExample/11Range/range.go
+++ Exercicios/GoByExample/11Range/range.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 
+	// um slice
 	nums := []int{2, 3, 4}
 	sum := 0
+
+	// range usado para somar numeros de um slice. (também funcionaria com array)
 	for _, num := range nums {
 		sum += num
 	}
