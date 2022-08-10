471ec309b6273eb149300b3ee20f3a0dd89a5974
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-10 15:35:20 -0300

    [doc] go by example exercicio 16 pointers

--- Exercicios/GoByExample/17Pointers/pointers.go
+++ Exercicios/GoByExample/17Pointers/pointers.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+// pointers permitem passar e trabalhar com referências a valores
+
+// essa função recebe um inteiro, então os argumentos serão passados como valor
+// zeroval receberá uma cópia do valor com o qual ela for chamada
 func zeroval(ival int) {
 	ival = 0
 }
 
+// zeroptr possui um parametro do tipo ponteiro para inteiro
+// dessa forma zoroptr receberá um endereço de memória da variável com a qual for chamada
+// permitindo assim alterar o valor que essa memória/variável armazena
 func zeroptr(iptr *int) {
 	*iptr = 0
 }
@@ -17,9 +24,13 @@ func main() {
 	zeroval(i)
 	fmt.Println("zeroval:", i) // 1
 
+	// a sintaxe &i passa o endereço da variável i, ou seja um ponteiro para i
 	zeroptr(&i)
 	fmt.Println("zeroptr:", i) // 0
 
+	// pointers podem ser impressos:
 	fmt.Println("pointer:", &i) // 0x0000160d8
 
+	// zeroval não altera o valor de i em main
+	// por outro lado zeroptr altera pois recebe o endereço em memória da variável
 }
