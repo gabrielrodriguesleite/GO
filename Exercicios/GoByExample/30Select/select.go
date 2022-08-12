541ec08beb65b95f2088e1f6735ddc6217fdae5f
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-11 23:38:33 -0300

    [doc] go by example exercicio 30 select

--- Exercicios/GoByExample/30Select/select.go
+++ Exercicios/GoByExample/30Select/select.go
@@ -9,9 +9,13 @@ import (
 // Combinar goroutines e channels com select é uma ferramenta poderosa de go
 func main() {
 
+	// Neste exemplo vamos usar select entre 2 channels
 	c1 := make(chan string)
 	c2 := make(chan string)
 
+	// Cada channel vai receber um valor apos um tempo para
+	// simular por exemplo uma operação bloqueante RPC
+	// executando em goroutines concorrentes
 	go func() {
 		time.Sleep(1 * time.Second)
 		c1 <- "one"
@@ -22,6 +26,8 @@ func main() {
 		c2 <- "two"
 	}()
 
+	// Usa-se select para aguardar abos os valores simultaneamente
+	// imprimido cada um assim que chegue
 	for i := 0; i < 2; i++ {
 		select {
 		case msg1 := <-c1:
@@ -30,5 +36,10 @@ func main() {
 			fmt.Println("received", msg2)
 		}
 	}
+
+	// recebemos o valor "one" e então "two" como esperado
+
+	// o tempo total da execução deve estar em torno de 2s
+	//  como ambos Sleep's executam concorrentes
 
 }
