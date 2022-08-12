3568dfbf91cde2bbc6fe7634006119eea9aa3518
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-11 23:48:19 -0300

    [doc] go by example exercicio 31 timeouts como implementar

--- Exercicios/GoByExample/31Timeouts/timeouts.go
+++ Exercicios/GoByExample/31Timeouts/timeouts.go
@@ -10,6 +10,11 @@ import (
 // Implementar timeouts em  go é facil e elegante graças a channels e select
 func main() {
 
+	// Nesse exemplo suponha que nós estamos executando uma chamada externa
+	// que retorna seu resultado no channel c1 após 2 segundos
+	// Note que o channel é buffered então o envio na goroutine não é bloqueante
+	// Este é um padrão comum para prevenir que goroutine vaze em caso do
+	// channel nunca ser lido
 	c1 := make(chan string, 1)
 	go func() {
 		time.Sleep(2 * time.Second)
