e9d661343a1839f8c51c310df42f2d63a893e300
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-11 23:17:05 -0300

    [doc] go by example exercicio 29 Channel Directions

--- Exercicios/GoByExample/29ChannelDirections/channel-directions.go
+++ Exercicios/GoByExample/29ChannelDirections/channel-directions.go
@@ -2,12 +2,20 @@ package main
 
 import "fmt"
 
+// É possível definir a finalidade do channel quando definimos uma função.
+// Se deve ser de entrada ou de saída. Isso melhora a tipagem da aplicação.
+
+// Esta função apenas caeita um channel que envie valores
+// causando um erro de compilação ao tentar receber um valor por meio deste
+// channel
 func ping(pings chan<- string, msg string) {
 
 	pings <- msg
 
 }
 
+// Esta função aceita um channel que recebe valores (pings)
+// e um segundo que envia (pongs)
 func pong(pings <-chan string, pongs chan<- string) {
 
 	msg := <-pings
