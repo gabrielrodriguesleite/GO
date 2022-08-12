5740d5f329bacb2d018a24f421ea98a7299e4a4c
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-11 22:29:17 -0300

    [doc] go by example exercicio 25 goroutines

--- Exercicios/GoByExample/25Goroutines/goroutines.go
+++ Exercicios/GoByExample/25Goroutines/goroutines.go
@@ -1,5 +1,6 @@
 package main
 
+// Uma goroutine é uma linha de execução leve
 import (
 	"fmt"
 	"time"
@@ -13,15 +14,28 @@ func f(from string) {
 
 func main() {
 
+	// Suponha que temos uma função call `f(s)`
+	// Aqui é como chamamos ela normalmente de maneira síncrona
 	f("direct")
 
+	// Para invocar numa goroutine chamamos com `go f(s)`
+	// Esta nova goroutine vai executar de modo concorrente com a primeira chamada
 	go f("goroutine")
 
+	// É possível também invocar uma goroutine usando uma função anônima
 	go func(msg string) {
 		fmt.Println(msg)
 	}("going")
 
+	// As duas chamadas da função são executadas de forma assíncrona em
+	// goroutines separadas. Aqui aguardamos para que terminem.
+	// (Uma forma melhor seria usando WaitGroup)
 	time.Sleep(time.Second)
 	fmt.Println("done")
 
 }
+
+// Quando esse código é executado vemos a execução da primeira chamada
+// bloqueante primeiro, então a saída das duas goroutines.
+// A saída das duas goroutines devem ser intercaladas por que as goroutines
+// rodam de maneira concorrentes.
