d2f639f553170e561b04fd3538a30c9f41eb9f34
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-11 23:44:19 -0300

    [doc] go by example exercicio 31 timeouts

--- Exercicios/GoByExample/31Timeouts/timeouts.go
+++ Exercicios/GoByExample/31Timeouts/timeouts.go
@@ -1,11 +1,21 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
+// Timeouts são importantes que se conectam com recursos externos
+// ou que estejam vinculados de outra forma ao tempo de execução
 func main() {
 
 	c1 := make(chan string, 1)
 	go func() {
 		time.Sleep(2 * time.Second)
 	}()
+
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	}
 }
