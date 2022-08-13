da1d286d64c71fbcdb3302178414b37b874efb1f
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-13 00:05:20 -0300

    [doc] go by example exercicio 35 timers | explicando timer sleep

--- Exercicios/GoByExample/35Timers/timers.go
+++ Exercicios/GoByExample/35Timers/timers.go
@@ -11,11 +11,18 @@ import (
 
 func main() {
 
+	// Timers representam um evento único no futuro.
+	// Indicamos quanto tempo deve aguradar e ele entrega um canal que será
+	// notificado quando completar o tempo. Este timer aguarda 2 segundos.
 	timer1 := time.NewTimer(2 * time.Second)
 
+	// O <-timer1.C bloqueia a execução até que a notificação do timer seja lançada.
 	<-timer1.C
 	fmt.Println("Timer 1 fired")
 
+	// Para o caso de simplesmente esperar é possível usar o time.Sleep.
+	// Uma das razões do timer ser tão útil é a capacidade de cancelar
+	// o timer antes dele disparar.
 	timer2 := time.NewTimer(time.Second)
 	go func() {
 		<-timer2.C
