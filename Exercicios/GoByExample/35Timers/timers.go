0fe8af2a9ede2dd3242c0d904d918c07e5f26f11
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-12 23:52:53 -0300

    [doc] go by example exercicio 35 timers

--- Exercicios/GoByExample/35Timers/timers.go
+++ Exercicios/GoByExample/35Timers/timers.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+// As vezes precisamos executar um código depois de um período ou
+// repetidamente com o mesmo intervalo. Go possui na biblioteca time
+// as funções timer e ticker, ambas feitas para tornar essas tarefas mais fáceis.
+
 func main() {
 
 	timer1 := time.NewTimer(2 * time.Second)
