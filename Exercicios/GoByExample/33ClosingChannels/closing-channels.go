f9ea47258ad093319eaa0bffe1f9af8a0c013e10
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-12 23:28:44 -0300

    [doc] go by example exercicio 33 closing channels | saída antiga (<1.18?)

--- Exercicios/GoByExample/33ClosingChannels/closing-channels.go
+++ Exercicios/GoByExample/33ClosingChannels/closing-channels.go
@@ -31,3 +31,16 @@ func main() {
 	<-done
 
 }
+
+/*
+ 	SAÍDA DO EXEMPLO MAS A PARTIR DA V 1.18 SAI DIFERENTE INCLUSIVE NO PLAYGROUND
+$ go run closing-channels.go
+sent job 1
+received job 1
+sent job 2
+received job 2
+sent job 3
+received job 3
+sent all jobs
+received all jobs
+*/
