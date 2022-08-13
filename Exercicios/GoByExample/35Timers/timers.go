7ebcfd1414e76009368c5a7ece5bda1f2e02df2c
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-12 23:55:23 -0300

    [feat] go by example exercicio 35 timers | timer 1 e timer 2

--- Exercicios/GoByExample/35Timers/timers.go
+++ Exercicios/GoByExample/35Timers/timers.go
@@ -16,5 +16,16 @@ func main() {
 	<-timer1.C
 	fmt.Println("Timer 1 fired")
 
-	// time.Sleep(2 * time.Second)
+	timer2 := time.NewTimer(time.Second)
+	go func() {
+		<-timer2.C
+		fmt.Println("Timer 2 fired")
+	}()
+
+	stop2 := timer2.Stop()
+	if stop2 {
+		fmt.Println("Timer 2 stopped")
+	}
+
+	time.Sleep(2 * time.Second)
 }
