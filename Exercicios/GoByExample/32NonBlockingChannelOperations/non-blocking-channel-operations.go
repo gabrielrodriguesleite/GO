5988233848adfcb797c63b2e59018c4c6dd6f672
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-12 23:01:44 -0300

    [doc] go by example exercicio 32 non blocking channel operations

--- Exercicios/GoByExample/32NonBlockingChannelOperations/non-blocking-channel-operations.go
+++ Exercicios/GoByExample/32NonBlockingChannelOperations/non-blocking-channel-operations.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
+// Por padrão enviar e receber por channels são operações bloqueantes.
+// Contudo, por usar um select com caso default implementamos
+// envios e recepções não bloqueantes, mesmo para select
+// multi direcionais sem bloqueios.
+
 func main() {
+
+	messages := make(chan string)
+	// signals := make(chan bool)
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message received")
+	}
 
 }
