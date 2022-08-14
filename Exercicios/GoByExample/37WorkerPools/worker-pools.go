cfb7a30a0b48685aa7e64c3288b1f53194b03155
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-13 22:11:42 -0300

    [feat] go by example exercicio 37 Worker Pools

--- Exercicios/GoByExample/37WorkerPools/worker-pools.go
+++ Exercicios/GoByExample/37WorkerPools/worker-pools.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func worker(id, int, jobs <-chan int, results chan<- int) {
+func worker(id int, jobs <-chan int, results chan<- int) {
 	for j := range jobs {
 		fmt.Println("worker", id, "started job", j)
 		time.Sleep(time.Second)
@@ -15,5 +15,23 @@ func worker(id, int, jobs <-chan int, results chan<- int) {
 }
 
 func main() {
+
+	const numJobs = 5
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs)
+
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	for j := 1; j <= numJobs; j++ {
+		jobs <- j
+	}
+
+	close(jobs)
+
+	for a := 1; a <= numJobs; a++ {
+		<-results
+	}
 
 }
