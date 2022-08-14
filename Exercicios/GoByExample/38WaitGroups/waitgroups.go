befcac8b1a633ef9eac796f5d0abf45e44a3bdee
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-13 22:45:42 -0300

    [doc] go by example exercicio 38 Wait Groups | main

--- Exercicios/GoByExample/38WaitGroups/waitgroups.go
+++ Exercicios/GoByExample/38WaitGroups/waitgroups.go
@@ -21,19 +21,33 @@ func main() {
 
 	fmt.Println("Inicio em:", time.Now())
 
+	// WaitGroup é usado para aguardar por todas goroutines lançadas
+	// finalizarem.
+	// *Se uma WaitGroup é explicitamente passada para funções, isso
+	// deve ser feito por ponteiro (by pointer)
 	var wg sync.WaitGroup
 
+	// Lançar 5 goroutines e incrementar o contador do WaitGroup
+	// uma vez por goroutine lançada.
 	for i := 1; i <= 5; i++ {
 		wg.Add(1)
 
+		// Para evitar o uso do mesmo valor de i em cada goroutine fechamento
+		// Para saber mais: https://golang.org/doc/faq#closures_and_goroutines
 		i := i
 
+		// Envolver a chamada do worker para certificar que WaitGroup será avisado
+		// que este worker finalizou.
+		// Desta forma o worker não tem qe ser avisado sobre os concorrentes primitivos
+		// involvidos na sua execução.
 		go func() {
 			defer wg.Done()
 			worker(i)
 		}()
 	}
 
+	// Bloqueia até que o contador do WaitGroup volte a zero.
+	// Todos os workers notificaram que foram completados.
 	wg.Wait()
 
 	fmt.Println("Fim em:", time.Now())
