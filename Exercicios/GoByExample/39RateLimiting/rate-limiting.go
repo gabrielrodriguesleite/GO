696eb9c02250a0c4f976a20c6acccd5d43075a6a
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-13 22:58:42 -0300

    [doc] go by example exercicio 39 Rate Limiting

--- Exercicios/GoByExample/39RateLimiting/rate-limiting.go
+++ Exercicios/GoByExample/39RateLimiting/rate-limiting.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+// Rate limiting é um importante mecanismo para controlar
+// uso de recursos e manter a qualidade do serviço.
+// Elegantemente go suporta rate limiting com goroutines,
+// channels e tickers.
 func main() {
 
 	requests := make(chan int, 5)
@@ -31,4 +35,14 @@ func main() {
 			burstyLimiter <- t
 		}
 	}()
+
+	burstyRequests := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		burstyRequests <- i
+	}
+	close(burstyRequests)
+	for req := range burstyRequests {
+		<-burstyLimiter
+		fmt.Println("request", req, time.Now())
+	}
 }
