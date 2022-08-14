bfb000abf6b6ed10ddeeb1915532a61ba6bf7623
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-13 22:21:02 -0300

    [doc] go by example exercicio 37 Worker Pools | main exeplicado

--- Exercicios/GoByExample/37WorkerPools/worker-pools.go
+++ Exercicios/GoByExample/37WorkerPools/worker-pools.go
@@ -23,22 +23,34 @@ func worker(id int, jobs <-chan int, results chan<- int) {
 
 func main() {
 
+	// Para usar a pool de workers precisamos enviar trabalhos e coletar
+	// resultados. Para isso usamos 2 canais, jobs e results.
 	const numJobs = 5
 	jobs := make(chan int, numJobs)
 	results := make(chan int, numJobs)
 
+	// Iniciamos 3 workers bloqueados pois se iniciam sem tarefas
 	for w := 1; w <= 3; w++ {
 		go worker(w, jobs, results)
 	}
 
+	// Então enviamos 5 serviços então fechamos o canal jobs indicando
+	// que estes são todos os serviços que faremos.
 	for j := 1; j <= numJobs; j++ {
 		jobs <- j
 	}
-
 	close(jobs)
 
+	// Finalmente coletamos todos os resultados dos trabalhos.
+	// Também garantimos que as goroutines workers finalizaram.
+	// Uma alternativa para aguardar multiplas goroutines é usando
+	// WaitGroup
 	for a := 1; a <= numJobs; a++ {
 		<-results
 	}
 
 }
+
+// Esse código executado mostra os 5 jobs sendo executados pelos
+// 3 workers. O programa leva em torno de 2 segundos ao invés de 5
+// por conta de estar sendo executado paralelamente por 3 workers
