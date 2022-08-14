d0d789df76835de065bfb51195dffec3e904b2e9
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-13 21:58:24 -0300

    [doc] go by example exercicio 36 Tickers

--- Exercicios/GoByExample/36Tickers/tickers.go
+++ Exercicios/GoByExample/36Tickers/tickers.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+// Timers serve para quando queremos fazer algo uma vez no futuro.
+// Ticker são para quando queremos repetir alguma ação dentro de intervalos regulares.
+// Neste exemplo temos um tick que imprime um valor periodicamente até que o paremos.
+
 func main() {
 
 	ticker := time.NewTicker(500 * time.Millisecond)
