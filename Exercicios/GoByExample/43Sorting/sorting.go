5bbf76796543cdca8762684073b371e02ab92faa
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-14 22:01:01 -0300

    [feat] go by example exercicio 43 Sorting | Int is sorted function

--- Exercicios/GoByExample/43Sorting/sorting.go
+++ Exercicios/GoByExample/43Sorting/sorting.go
@@ -13,5 +13,8 @@ func main() {
 
 	ints := []int{7, 2, 4}
 	sort.Ints(ints)
-	fmt.Println("ints:", ints)
+	fmt.Println("ints:   ", ints)
+
+	s := sort.IntsAreSorted(ints)
+	fmt.Println("Sorted: ", s)
 }
