cb7055a8f72c5d884988fba7130122630c21d41f
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-14 22:45:18 -0300

    [doc] go by example exercicio 45 Panic | por que usar

--- Exercicios/GoByExample/45Panic/panic.go
+++ Exercicios/GoByExample/45Panic/panic.go
@@ -1,6 +1,12 @@
 package main
 
+// Um panic tipicamente é quando algo ocorre de uma forma inesperadamente errada
+// Na maioria das vezes é usado para abortar a execução quando ocorrem erros que
+// não deveriam de acontecer numa operação normal, ou que o código não está
+// preparado para lidar.
 func main() {
 
+	// Panic será usado nos próximos exemplos para verificar por erros inesperados
+	// Esta é a única aplicação nesses exemplos desenhada para disparar um panic
 	panic("a problem")
 }
