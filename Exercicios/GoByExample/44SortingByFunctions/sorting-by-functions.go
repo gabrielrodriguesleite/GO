9ba84e43a13a26c7304fd3edf075d671c6c858e3
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-14 22:24:29 -0300

    [doc] go by example exercicio 44 Sort by Function | Explicação da implementação da interface de sort.Sort

--- Exercicios/GoByExample/44SortingByFunctions/sorting-by-functions.go
+++ Exercicios/GoByExample/44SortingByFunctions/sorting-by-functions.go
@@ -5,8 +5,25 @@ import (
 	"sort"
 )
 
+// As vezes queremos ordenar uma coleção de alguma forma
+// diferente da ordem natural.
+// Por exemplo, supondo que queremos ordenar strings por
+// seu comprimento ao invés da ordem alfabética.
+// Aqui temos um exemplo desta função de ordenação
+// personalizada em Go
+
+// Para criar a função de ordenação personalizada em Go
+// é necessário definirmos um tipo correspondente.
+// Aqui creamos o tipo byLength que possui o nome da
+// operação que desejamos e definimos o tipo dos dados.
 type byLength []string
 
+// Para usarmos a função genérica Sort do pacote sort precisamos
+// implementar os os métodos da interface sort.Interface - Len,
+// Less e Swap. Len e Swap vão ser similares para muitos tipos
+// e Less vai conter a função personalizada de ordenação.
+// Neste exemplo queremos ordenar de maneira crescente pelo
+// comprimento da string, então usamos len(s[i]) e len(s[j])
 func (s byLength) Len() int {
 	return len(s)
 }
