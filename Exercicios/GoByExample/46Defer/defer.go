eb719e5717860fb964b42bf776ea1ad12ee7217d
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-14 23:17:34 -0300

    [doc] go by example exercicio 46 Defer | fechando um arquivo com defer

--- Exercicios/GoByExample/46Defer/defer.go
+++ Exercicios/GoByExample/46Defer/defer.go
@@ -5,6 +5,28 @@ import (
 	"os"
 )
 
+// defer = adiar/postergar
+// defer é usado para garantir que uma função é chamada como último
+// passo de uma função que está finalizando, geralmente com o intuito
+// de limpeza.
+// defer é também usado onde ex: ensure e finally seriam usadas em
+// outras linguagens.
+
+// Supondo que queremos criar um arquivo, escrever nele e então
+// fechá-lo quando terminar. Aqui temos um exemplo desses passos
+// utilizando defer.
+func main() {
+
+	// Imediatamente após pegar um objeto arquivo com createFile
+	// chamamos defer para fechar o arquivo com closeFile
+	// Com isso garantimos que closeFile será chamado após writeFile
+	// ter finalizado e antes do fechamento de main.
+	f := createFile("/tmp/defer.txt")
+	defer closeFile(f)
+	writeFile(f)
+
+}
+
 func createFile(p string) *os.File {
 	fmt.Println("criando")
 	f, err := os.Create(p)
@@ -19,6 +41,8 @@ func writeFile(f *os.File) {
 	fmt.Fprintln(f, "dados")
 }
 
+// É sempre importante verificar por erros quando fechamos arquivos
+// mesmo em funções defer
 func closeFile(f *os.File) {
 	fmt.Println("fechando")
 	err := f.Close()
@@ -28,10 +52,5 @@ func closeFile(f *os.File) {
 	}
 }
 
-func main() {
-
-	f := createFile("/tmp/defer.txt")
-	defer closeFile(f)
-	writeFile(f)
-
-}
+// Rodando a aplicação podemos confirmar que o arquivo é fechado
+// após a escrita.
