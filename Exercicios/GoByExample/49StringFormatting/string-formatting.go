e16e71be4522ea352e8376f195197e7020f5a3c6
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-19 15:13:12 -0300

    [doc] go by examples | 49 String Formatting

--- Exercicios/GoByExample/49StringFormatting/string-formatting.go
+++ Exercicios/GoByExample/49StringFormatting/string-formatting.go
@@ -2,12 +2,23 @@ package main
 
 import "fmt"
 
+// go oferece um excelente suport para formatação de strings seguindo
+// o modelo pringf. Neste código temos alguns exemplos.
+
 type point struct {
 	x, y int
 }
 
 func main() {
 	p := point{1, 2}
+
+	// go oferece varios "verbos" de formatação desenhados para formatar
+	// valorer comuns. Neste exemplo, imprimimos uma instancia da nossa
+	// estrutura point:
 	fmt.Printf("struct1: %v\n", p)
+
+	fmt.Printf("struct2: %+v\n", p)
+
+	fmt.Printf("struct3: %#v\n", p)
 
 }
