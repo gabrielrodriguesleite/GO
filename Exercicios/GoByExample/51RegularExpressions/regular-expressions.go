3e2e12a313da08a7e220536103d2ce247c7ca7cd
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-20 23:56:14 -0300

    [feat] go by examples | 51 expressões regulares - teste basico

--- Exercicios/GoByExample/51RegularExpressions/regular-expressions.go
+++ Exercicios/GoByExample/51RegularExpressions/regular-expressions.go
@@ -1,8 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+)
+
 // Go possui suporte nativo para expressões regulares.
 // Neste código alguns exemplos communs relacionados
 // a tarefas com expressões regulares.
 func main() {
+
+	match, _ := regexp.MatchString("p([a-z]+)ego", "pessego")
+	fmt.Println(match)
 
 }
