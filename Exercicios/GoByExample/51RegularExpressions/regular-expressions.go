676d9046c2b1ef15e9b48a548a11bd26d945a3fb
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-20 23:54:30 -0300

    [doc] go by examples | 51 expressões regulares

--- Exercicios/GoByExample/51RegularExpressions/regular-expressions.go
+++ Exercicios/GoByExample/51RegularExpressions/regular-expressions.go
@@ -1,5 +1,8 @@
 package main
 
+// Go possui suporte nativo para expressões regulares.
+// Neste código alguns exemplos communs relacionados
+// a tarefas com expressões regulares.
 func main() {
 
 }
