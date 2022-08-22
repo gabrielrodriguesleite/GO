88f80453060207f514598a2015f7b5ff2e72ab68
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-22 20:10:45 -0300

    [doc] go by examples | 52 JSON - exportando tipos personalizados (estruturas)

--- Exercicios/GoByExample/52JSON/json.go
+++ Exercicios/GoByExample/52JSON/json.go
@@ -3,11 +3,15 @@ package main
 // Go possui suporte integrado para codificação e decodificação de JSON
 // incluindo de e para tipos de dados integrados e personalizados.
 
+// Estas duas estruturas serão utilizadas para demonstrar codificação e
+// decodificação de tipos personalizados
 type response1 struct {
 	Page   int
 	Fruits []string
 }
 
+// Apenas campos exportados vão ser codificados/decodificados em JSON.
+// Campos devem iniciar com letras maiúsculas para serem exportados.
 type response2 struct {
 	Page   int      `json:"page"`
 	Fruits []string `json:"fruits"`
