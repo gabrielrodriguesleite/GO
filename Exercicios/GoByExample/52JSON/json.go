27fed4b8d7b9438403d3c7d459679d0559303450
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-22 21:06:18 -0300

    [doc] go by examples | 52 JSON - falta de pontuação

--- Exercicios/GoByExample/52JSON/json.go
+++ Exercicios/GoByExample/52JSON/json.go
@@ -76,14 +76,14 @@ func main() {
 	// ----------
 
 	// Decodificando dados JSON em valores GO.
-	// Aqui um exemlo de uma estrutura de dados genérica
+	// Aqui um exemplo de uma estrutura de dados genérica
 	byt := []byte(`{"num":3.14, "strs":["a", "b"]}`)
 
 	// É preciso providenciar uma variável onde o pacote JSON possa colocar os dados
 	// decodificados. Este map vai conter um map de strings para tipos de dados arbitrários.
 	var dat map[string]interface{}
 
-	// Aqui a decodificação e o tratamento de erro
+	// Aqui a decodificação e o tratamento de erro.
 	if err := json.Unmarshal(byt, &dat); err != nil {
 		panic(err)
 	}
