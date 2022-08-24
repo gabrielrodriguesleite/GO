e077fa04898f95a40febfcc03e8b3c67bfdb97f3
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-23 23:28:39 -0300

    [feat] go by examples | 54 Time - data/hora atual

--- Exercicios/GoByExample/54Time/time.go
+++ Exercicios/GoByExample/54Time/time.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 // Go possui um amplo suporte para tempo e períodos de tempo
 
 func main() {
+
+	p := fmt.Println
+
+	now := time.Now()
+	p(now)
 
 }
