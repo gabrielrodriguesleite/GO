2f2e113c63a290fac913d262ad0579ec4e91267f
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-23 23:47:27 -0300

    [doc] go by examples | 55 Epoch

--- Exercicios/GoByExample/55Epoch/epoch.go
+++ Exercicios/GoByExample/55Epoch/epoch.go
@@ -1,5 +1,9 @@
 package main
 
+// Uma necessidade comum ao programar é conseguir o número em segundos,
+// milisegundos ou nanosegundos desde uma Unix epoch.
+// Mais aqui: https://en.wikipedia.org/wiki/Unix_time
+
 func main() {
 
 }
