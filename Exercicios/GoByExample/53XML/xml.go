2d323722a96b3b77cdbcc899a64b9536b7cb6c48
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-23 22:54:54 -0300

    [doc] go by examples | 53 XML - diretivas de codificação/decodificação

--- Exercicios/GoByExample/53XML/xml.go
+++ Exercicios/GoByExample/53XML/xml.go
@@ -8,6 +8,12 @@ import (
 // Go vem com suporte imbutido para XML e formatos do tipo
 // através do pacote encoding.xml
 
+// Plant vai ser mapeada para XML. Do mesmo modo que no exemplo do JSON
+// as tags dos os campos contém diretivas para codificação/decodificação.
+// Aqui usamos algumas funções especiais do pacote XML: o campo XMLName
+// dita do nome do elemento XML que representa a estrutura;
+// id, attr, significa que o campo Id é um atributo XML ao invés de um
+// elemento aninhado.
 type Plant struct {
 	XMLName xml.Name `xml:"plant"`
 	Id      int      `xml:"id,attr"`
