2b60c82ede17a5c5010591aa8d802c64c1ee248e
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-23 23:25:10 -0300

    [doc] go by examples | 53 XML - exemplo de saída deste código

--- Exercicios/GoByExample/53XML/xml.go
+++ Exercicios/GoByExample/53XML/xml.go
@@ -63,3 +63,40 @@ func main() {
 	out, _ = xml.MarshalIndent(nesting, " ", "  ")
 	fmt.Println(string(out))
 }
+
+/* EXEMPLO DE SAÍDA DESTE CÓDIGO
+<plant id="27">
+   <name>Coffee</name>
+   <origin>Ethiopia</origin>
+   <origin>Brazil</origin>
+ </plant>
+
+// -----------------
+
+<?xml version="1.0" encoding="UTF-8"?>
+ <plant id="27">
+   <name>Coffee</name>
+   <origin>Ethiopia</origin>
+   <origin>Brazil</origin>
+ </plant>
+
+// -----------------
+
+Plant id=27, name=Coffee, origin=[Ethiopia Brazil]
+ <nesting>
+   <parent>
+     <child>
+       <plant id="27">
+         <name>Coffee</name>
+         <origin>Ethiopia</origin>
+         <origin>Brazil</origin>
+       </plant>
+       <plant id="81">
+         <name>Tomato</name>
+         <origin>Mexico</origin>
+         <origin>California</origin>
+       </plant>
+     </child>
+   </parent>
+ </nesting>
+*/
