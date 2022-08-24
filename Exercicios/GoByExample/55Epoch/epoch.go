78dd55328521dca5b9f49059240ed72c015d32f7
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-23 23:49:58 -0300

    [doc] go by examples | 55 Epoch - adquirindo o momento atual desde epoch

--- Exercicios/GoByExample/55Epoch/epoch.go
+++ Exercicios/GoByExample/55Epoch/epoch.go
@@ -1,9 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 // Uma necessidade comum ao programar é conseguir o número em segundos,
 // milisegundos ou nanosegundos desde uma Unix epoch.
 // Mais aqui: https://en.wikipedia.org/wiki/Unix_time
 
 func main() {
 
+	p := fmt.Println
+
+	now := time.Now()
+	p(now)
+
+	p(now.Unix())
+	p(now.UnixMilli())
+	p(now.UnixNano())
 }
