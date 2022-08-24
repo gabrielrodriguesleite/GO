d14e8fc096cbf2e72602cd92999dd541bb773025
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-23 22:49:28 -0300

    [feat] go by examples | 53 XML - Plant struct exemplo

--- Exercicios/GoByExample/53XML/xml.go
+++ Exercicios/GoByExample/53XML/xml.go
@@ -1,7 +1,16 @@
 package main
 
+import "encoding/xml"
+
 // Go vem com suporte imbutido para XML e formatos do tipo
 // através do pacote encoding.xml
+
+type Plant struct {
+	XMLName xml.Name `xml:"plant"`
+	Id      int      `xml:"id,attr"`
+	Name    string   `xml:"name"`
+	Origin  []string `xml:"origin"`
+}
 
 func main() {
 
