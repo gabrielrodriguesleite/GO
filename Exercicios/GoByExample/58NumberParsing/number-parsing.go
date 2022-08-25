be4470f2422f335457a792f5c91903ac161783ad
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-24 22:45:02 -0300

    [feat] go by examples | 58 Number Parsing - interpretando tipos básicosde números a partir de strings

--- Exercicios/GoByExample/58NumberParsing/number-parsing.go
+++ Exercicios/GoByExample/58NumberParsing/number-parsing.go
@@ -1,9 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // Analizar/interpretar números à partir de strings é uma tarefa
 // tanto comum como básica para muitas aplicações.
 // Aqui temos exemplos disso em Go:
 
 func main() {
+
+	f, _ := strconv.ParseFloat("1.234", 64)
+	fmt.Println(f)
+
+	i, _ := strconv.ParseInt("567", 0, 64)
+	fmt.Println(i)
+
+	d, _ := strconv.ParseInt("0x1c8", 0, 64)
+	fmt.Println(d)
+
+	u, _ := strconv.ParseUint("789", 0, 64)
+	fmt.Println(u)
 
 }
