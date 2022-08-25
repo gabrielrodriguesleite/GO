5d35e18a1c360d8808e008c52c0a788dbb16d559
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-24 22:07:30 -0300

    [doc] go by examples | 57 Random Numbers - tradução melhorada

--- Exercicios/GoByExample/57RandomNumbers/random-numbers.go
+++ Exercicios/GoByExample/57RandomNumbers/random-numbers.go
@@ -18,7 +18,7 @@ func main() {
 	// Este exemplo retorna um ponto flutuante de 0.0 à f < 1.0
 	fmt.Println(rand.Float64())
 
-	// Também é possível gerar um ponto flutuante em outra extenção
+	// Também é possível gerar um ponto flutuante em outra variação
 	// por exemplo 5.0 <= f < 10.0
 	fmt.Print((rand.Float64()*5)+5, ",")
 	fmt.Print((rand.Float64() * 5) + 5)
