fa85db04be36a88e7a19526d93ca955409c82833
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-24 22:49:16 -0300

    [doc] go by examples | 58 Number Parsing - interpretando tipos básicosde números a partir de strings

--- Exercicios/GoByExample/58NumberParsing/number-parsing.go
+++ Exercicios/GoByExample/58NumberParsing/number-parsing.go
@@ -11,15 +11,23 @@ import (
 
 func main() {
 
+	// O pacote integrado strconv provê intrerpretação de números.
+
+	// Com ParseFloat, convertemos um ponto flutuante, 64 define a
+	// precisão desejada.
 	f, _ := strconv.ParseFloat("1.234", 64)
 	fmt.Println(f)
 
+	// Para ParseInt, 0 significa inferir a base 0 e 64 solicita
+	// que o resultado caiba em 64 bits.
 	i, _ := strconv.ParseInt("567", 0, 64)
 	fmt.Println(i)
 
+	// ParseInt vai reconhecer o formato hexadecimal.
 	d, _ := strconv.ParseInt("0x1c8", 0, 64)
 	fmt.Println(d)
 
+	// Existe também a opção de converter um número não negativo
 	u, _ := strconv.ParseUint("789", 0, 64)
 	fmt.Println(u)
 
