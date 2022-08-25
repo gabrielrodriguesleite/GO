5cdf8ee712701c04704443c856b36dae3c14e035
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-24 22:41:19 -0300

    [doc] go by examples | 58 Number Parsing

---
+++ Exercicios/GoByExample/58NumberParsing/number-parsing.go
@@ -0,0 +1,9 @@
+package main
+
+// Analizar/interpretar números à partir de strings é uma tarefa
+// tanto comum como básica para muitas aplicações.
+// Aqui temos exemplos disso em Go:
+
+func main() {
+
+}
