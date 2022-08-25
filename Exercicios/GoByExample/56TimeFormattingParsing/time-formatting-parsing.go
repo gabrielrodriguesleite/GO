ab70bdcb6ae53de7397dcde0905e066c9d32d206
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-24 21:38:37 -0300

    [doc] go by examples | 56 Time Formatting / Parsing - Parsing RFC3339

--- Exercicios/GoByExample/56TimeFormattingParsing/time-formatting-parsing.go
+++ Exercicios/GoByExample/56TimeFormattingParsing/time-formatting-parsing.go
@@ -5,15 +5,19 @@ import (
 	"time"
 )
 
-// Go suporta formatação e tradução de formatos de tempo
-// via layouts baseados em padrão
+// Go suporta formatação e análise de formatos de tempo
+// via layouts baseados em padrão.
 
 func main() {
 
 	p := fmt.Println
 
 	// Este é um exemplo básico de formatação de tempo de acordo com
-	// RFC3339, usando a constante layout correspondente
+	// RFC3339, usando a constante do perfil correspondente.
 	t := time.Now()
 	p(t.Format(time.RFC3339))
+
+	// Análise de tempo utiliza o mesmo valor de perfil como formato.
+	t1, _ := time.Parse(time.RFC3339, "2021-11-01T22:08:41+00:00")
+	p(t1)
 }
