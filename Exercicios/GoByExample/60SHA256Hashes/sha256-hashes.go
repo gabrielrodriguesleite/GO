5d1b254fb1ca5db2f7d3054e54cadeeaef182dc0
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-25 22:51:18 -0300

    [doc] go by example | 60 SHA256 Hashes - gerando um hash a partir de string

--- Exercicios/GoByExample/60SHA256Hashes/sha256-hashes.go
+++ Exercicios/GoByExample/60SHA256Hashes/sha256-hashes.go
@@ -10,14 +10,21 @@ import (
 // SHA256 para computar assinaturas de certificados. Neste exemplo temos uma
 // forma de computar hashes SHA256 em Go.
 
+// Go implementa várias funções hash nos pacotes crypto/*
 func main() {
 
 	s := "sha256 this string"
 
+	// Aqui instanciamos um novo hash
 	h := sha256.New()
 
+	// Write espera bytes. Se temos uma string s, usamos
+	// []byte(s) para transformar em bytes.
 	h.Write([]byte(s))
 
+	// Aqui recebemos a hash resultante como um slice de bytes.
+	// O argumento de Sum pode ser usado para juntar a um slice de bytes existente.
+	// Normalmente o argumento não é necessário.
 	bs := h.Sum(nil)
 
 	fmt.Println(s)
