7271c793ee3676e9208428429bf5a6f65926fe3c
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-25 22:37:55 -0300

    [doc] go by example | 59 URL parsing - saída esperada p/ print

--- Exercicios/GoByExample/59URLParsing/url-parsing.go
+++ Exercicios/GoByExample/59URLParsing/url-parsing.go
@@ -27,30 +27,30 @@ func main() {
 
 	// User contém todas as informações de autenticação.
 	// Também é possível acessar os valores de nome e senha de forma individual.
-	fmt.Println(u.User)
-	fmt.Println(u.User.Username())
-	p, _ := u.User.Password()
-	fmt.Println(p)
+	fmt.Println(u.User)            // user:pass
+	fmt.Println(u.User.Username()) // user
+	p, _ := u.User.Password()      //
+	fmt.Println(p)                 // pass
 
 	// Host contém  tanto o hostname e a porta se presentes.
 	// Para extraí-los se usa SplitHostPort.
-	fmt.Println(u.Host)
-	host, port, _ := net.SplitHostPort(u.Host)
-	fmt.Println(host)
-	fmt.Println(port)
+	fmt.Println(u.Host)                        // host.com:5432
+	host, port, _ := net.SplitHostPort(u.Host) //
+	fmt.Println(host)                          // host.com
+	fmt.Println(port)                          // 5432
 
 	// Extraindo o path e o fragmento que vem depois de #
-	fmt.Println(u.Path)
-	fmt.Println(u.Fragment)
+	fmt.Println(u.Path)     // /path
+	fmt.Println(u.Fragment) // f
 
 	// Para acessar os parametros em uma string no formato k=v, usasse RawQuery.
 	// Também é possível analisar parametros de consulta para um map.
 	// O map de parametros de consulta analisado é de strings para slices de strings
 	// então o index em [0] é para o caso de acessar apenas o primeiro valor.
-	fmt.Println(u.RawQuery)
-	m, _ := url.ParseQuery(u.RawQuery)
-	fmt.Println(m)
-	fmt.Println(m["k"][0])
+	fmt.Println(u.RawQuery)            // k=v
+	m, _ := url.ParseQuery(u.RawQuery) //
+	fmt.Println(m)                     // map[k:[v]]
+	fmt.Println(m["k"][0])             // v
 
 }
 
