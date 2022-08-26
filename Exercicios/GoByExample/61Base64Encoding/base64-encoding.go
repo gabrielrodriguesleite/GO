22489e07c88c4fd4bdfb01d12f49eb9997062de6
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-25 23:01:11 -0300

    [feat] go by example | 61 Base64 Encoding - codificando string em base64

--- Exercicios/GoByExample/61Base64Encoding/base64-encoding.go
+++ Exercicios/GoByExample/61Base64Encoding/base64-encoding.go
@@ -1,8 +1,18 @@
 package main
 
+import (
+	"encoding/base64"
+	"fmt"
+)
+
 // Go provê suporte integrado para codificação/decodificação base64
 // Mais em: https://en.wikipedia.org/wiki/Base64
 
 func main() {
+
+	data := "abc123!?$*&()'-=@~"
+
+	sEnc := base64.StdEncoding.EncodeToString([]byte(data))
+	fmt.Println(sEnc)
 
 }
