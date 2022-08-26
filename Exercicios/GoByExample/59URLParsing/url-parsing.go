070c0f0d4e4407d270e73752ec6abca08ef27a8f
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-25 22:17:55 -0300

    [feat] go by example | 59 URL parsing

--- Exercicios/GoByExample/59URLParsing/url-parsing.go
+++ Exercicios/GoByExample/59URLParsing/url-parsing.go
@@ -1,8 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/url"
+)
+
 // URLs servem para prover uma forma uniforme para localizar recursos.
 // Mais em: https://adam.herokuapp.com/past/2010/3/30/urls_are_the_uniform_way_to_locate_resources/
 // Aqui alguns exemplos de como analizar URLs em Go.
 func main() {
+
+	s := "postgres://user:pass@host.com:5432/path?k=v#f"
+
+	u, err := url.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(u.Scheme)
 
 }
