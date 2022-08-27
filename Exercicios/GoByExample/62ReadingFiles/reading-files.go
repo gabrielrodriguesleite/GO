4769a149a058040581990a2f2c248eadda2cb2bd
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-26 21:39:40 -0300

    [feat] go by example | 62 Reading files - lendo a partir de uma posição

--- Exercicios/GoByExample/62ReadingFiles/reading-files.go
+++ Exercicios/GoByExample/62ReadingFiles/reading-files.go
@@ -36,4 +36,12 @@ func main() {
 	check(err)
 	fmt.Printf("%d bytes: %s\n", n1, string(b1[:n1]))
 
+	o2, err := f.Seek(6, 0)
+	check(err)
+	b2 := make([]byte, 2)
+	n2, err := f.Read(b2)
+	check(err)
+	fmt.Printf("%d bytes @ %d: ", n2, o2)
+	fmt.Printf("%v\n", string(b2[:n2]))
+
 }
