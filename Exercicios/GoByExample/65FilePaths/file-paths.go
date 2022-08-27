8a4e098b35137fec2431d19cecde3d116a643acb
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-27 20:07:31 -0300

    [doc] go by example | 65 File Paths

---
+++ Exercicios/GoByExample/65FilePaths/file-paths.go
@@ -0,0 +1,8 @@
+package main
+
+// O pacote filepath possui funções para analisar e contruir caminhos de arquivos
+// de forma portável entre sistemas operacionais. Por exemplo dir/file no linux
+// e dir\file no windows.
+func main() {
+
+}
