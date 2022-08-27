329231885662aa2296f7d3b25908e0b4d68595df
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-26 21:43:28 -0300

    [feat] go by example | 62 Reading files - lendo a partir de um ponto determinado uma quantidade de bytes determinada

--- Exercicios/GoByExample/62ReadingFiles/reading-files.go
+++ Exercicios/GoByExample/62ReadingFiles/reading-files.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -45,4 +46,10 @@ func main() {
 	fmt.Printf("%d bytes @ %d: ", n2, o2)
 	fmt.Printf("%v\n", string(b2[:n2]))
 
+	o3, err := f.Seek(6, 0)
+	check(err)
+	b3 := make([]byte, 2)
+	n3, err := io.ReadAtLeast(f, b3, 2)
+	check(err)
+	fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
 }
