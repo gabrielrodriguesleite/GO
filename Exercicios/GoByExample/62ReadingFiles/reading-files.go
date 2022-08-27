5ed6c3baa8e5a35aaf2490e0c2740ade1a4c442e
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-26 21:24:18 -0300

    [doc] go by example | 62 Reading files - helper for errors

--- Exercicios/GoByExample/62ReadingFiles/reading-files.go
+++ Exercicios/GoByExample/62ReadingFiles/reading-files.go
@@ -3,6 +3,8 @@ package main
 // Ler e escrever arquivos são tarefas básicas porém necessárias para muitos
 // programas Go. Primeiramente veremos exemplos de leitura.
 
+// Ler arquivos envolve verificar por erros com frequência. Esta função
+// irá ajudar nessa tarefa.
 func check(e error) {
 	if e != nil {
 		panic(e)
