cf59f0afa4d10894828e4199a91c2e4ad7025273
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-26 22:02:01 -0300

    [feat] go by example | 63 Writing Files - exemplo de criação e escrita em arquivo com controle maior

--- Exercicios/GoByExample/63WritingFiles/writing-files.go
+++ Exercicios/GoByExample/63WritingFiles/writing-files.go
@@ -1,6 +1,9 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 // Escrever arquivo usando Go é uma tarefa parecida com a de leitura
 // demonstrada no exemplo anterior
@@ -18,5 +21,15 @@ func main() {
 	d1 := []byte("hello\ngo\n")
 	err := os.WriteFile("/tmp/dat1", d1, 0644)
 	check(err)
+
+	f, err := os.Create("/tmp/dat2")
+	check(err)
+
+	defer f.Close()
+
+	d2 := []byte{115, 111, 109, 101, 10}
+	n2, err := f.Write(d2)
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n2)
 
 }
