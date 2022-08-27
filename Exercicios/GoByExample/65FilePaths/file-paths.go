534d9091d8e401bf6d23d0eba05f5b7294c270de
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-27 20:57:19 -0300

    [doc] go by example | 65 File Paths - Saídas esperadas e saídas dos prints

--- Exercicios/GoByExample/65FilePaths/file-paths.go
+++ Exercicios/GoByExample/65FilePaths/file-paths.go
@@ -14,31 +14,31 @@ func main() {
 	// Join deve ser usado quando se constroem paths de forma portável.
 	// Aceita n número de argumentos e constroe a hierarquia do caminho à partir destes.
 	p := filepath.Join("dir1", "dir2", "filename")
-	fmt.Println("p:", p)
+	fmt.Println("p:", p) // p: dir1/dir2/filename
 
 	// É importante sempre dar preferência ao Join ao invés de concatenar caminhos
 	// manualmente com /s ou \s. Além de prover portabilidade, Join também normaliza
 	// os caminhos por remover separadores e mudanças de pastas desnecessárias.
-	fmt.Println(filepath.Join("dir1//", "filename"))
-	fmt.Println(filepath.Join("dir1/../dir1", "filename"))
+	fmt.Println(filepath.Join("dir1//", "filename"))       // dir1/filename
+	fmt.Println(filepath.Join("dir1/../dir1", "filename")) // dir1/filename
 
 	// Dir e Base podem ser usados para separar um caminho em pasta e arquivo.
 	// De maneira alternativa, Split irá retornar ambos na mesma chamada:
-	fmt.Println("Dir(p):", filepath.Dir(p))
-	fmt.Println("Base(p):", filepath.Base(p))
+	fmt.Println("Dir(p):", filepath.Dir(p))   // Dir(p): dir1/dir2
+	fmt.Println("Base(p):", filepath.Base(p)) // Base(p): filename
 
 	f, d := filepath.Split(p)
-	fmt.Printf("Split(p) → Dir: %s, Base: %s\n", f, d)
+	fmt.Printf("Split(p) → Dir: %s, Base: %s\n", f, d) // Split(p) → Dir: dir1/dir2/, Base: filename
 
 	// É possível também testar se um caminho é relativo ou absoluto.
-	fmt.Println(filepath.IsAbs("dir/file"))
-	fmt.Println(filepath.IsAbs("/dir/file"))
+	fmt.Println(filepath.IsAbs("dir/file"))  // false
+	fmt.Println(filepath.IsAbs("/dir/file")) // true
 
 	// Alguns arquivos possuem nomes com a extenção indicada após o ponto.
 	// É possível dividir a extenção e capturar ela com Ext:
 	filename := "config.json"
 	ext := filepath.Ext(filename)
-	fmt.Println(ext)
+	fmt.Println(ext) // .json
 
 	// strings.TrimSuffix é útil para remover a extenção do nome do arquivo.
 	fmt.Println(strings.TrimSuffix(filename, ext))
@@ -50,13 +50,28 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(rel)
+	fmt.Println(rel) // t/file
 
 	rel, err = filepath.Rel("a/b", "a/c/t/file")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(rel)
+	fmt.Println(rel) // ../c/t/file
 
 }
+
+/* SAÍDA ESPERADA PARA O CÓDIGO
+p: dir1/dir2/filename
+dir1/filename
+dir1/filename
+Dir(p): dir1/dir2
+Base(p): filename
+Split(p) → Dir: dir1/dir2/, Base: filename
+false
+true
+.json
+config
+t/file
+../c/t/file
+*/
