19a0b8172c6d9405d31c072ae7bb26c3993218a7
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-27 20:12:17 -0300

    [feat] go by example | 65 File Paths - exemplo simples do método join do pacote filepath

--- Exercicios/GoByExample/65FilePaths/file-paths.go
+++ Exercicios/GoByExample/65FilePaths/file-paths.go
@@ -1,8 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"path/filepath"
+)
+
 // O pacote filepath possui funções para analisar e contruir caminhos de arquivos
 // de forma portável entre sistemas operacionais. Por exemplo dir/file no linux
 // e dir\file no windows.
 func main() {
+	p := filepath.Join("dir1", "dir2", "filename")
+	fmt.Println("p:", p)
 
 }
