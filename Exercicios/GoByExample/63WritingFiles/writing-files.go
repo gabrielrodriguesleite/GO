49eaf15e9c4840a38dae69e5f0f64a3fa9adbb87
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-26 21:56:51 -0300

    [doc] go by example | 63 Writing Files - error helper

--- Exercicios/GoByExample/63WritingFiles/writing-files.go
+++ Exercicios/GoByExample/63WritingFiles/writing-files.go
@@ -3,6 +3,7 @@ package main
 // Escrever arquivo usando Go é uma tarefa parecida com a de leitura
 // demonstrada no exemplo anterior
 
+// Novamente é muito útil ter uma função para tratar a verificação por erros
 func check(e error) {
 	if e != nil {
 		panic(e)
