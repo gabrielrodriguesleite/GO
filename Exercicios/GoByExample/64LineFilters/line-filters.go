9438fdb0d475d53090e341ede98735824d7d8d63
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-27 19:15:12 -0300

    [doc] go by example | 64 Line Filters

---
+++ Exercicios/GoByExample/64LineFilters/line-filters.go
@@ -0,0 +1,9 @@
+package main
+
+// Line filter é um tipo de programa comum que lê a entrada do stdin,
+// processa a entrada e então imprime o resultado para o stdout.
+// Exemplos desses tipos são grep e sed
+
+func main() {
+
+}
