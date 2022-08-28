32ba25f5250239dd40c730b6d1709fee18c1b55f
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-27 21:03:19 -0300

    [doc] go by example | 66 Directories - criando subdiretório temporário e agendando sua exclusão

--- Exercicios/GoByExample/66Directories/directories.go
+++ Exercicios/GoByExample/66Directories/directories.go
@@ -11,9 +11,13 @@ func check(e error) {
 // Go possui varias funções úteis para trabalhar com pastas no sistema de arquivos
 func main() {
 
+	// Criando um subdiretório no diretório atual.
 	err := os.Mkdir("subdir", 0755)
 	check(err)
 
+	// Quando se está trabalhando com diretórios temporários é uma boa prática
+	// agendar sua remoção. os.RemoveAll vai deletar uma árvore de diretório
+	// inteiro similar ao "rm -rf".
 	defer os.RemoveAll("subdir")
 
 }
