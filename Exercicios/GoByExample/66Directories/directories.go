30528d3d8876afe07dbba3d33744e067f33f018f
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-27 21:01:04 -0300

    [doc] go by example | 66 Directories - criando subdiretório no diretório atual

--- Exercicios/GoByExample/66Directories/directories.go
+++ Exercicios/GoByExample/66Directories/directories.go
@@ -1,5 +1,7 @@
 package main
 
+import "os"
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -8,5 +10,10 @@ func check(e error) {
 
 // Go possui varias funções úteis para trabalhar com pastas no sistema de arquivos
 func main() {
+
+	err := os.Mkdir("subdir", 0755)
+	check(err)
+
+	defer os.RemoveAll("subdir")
 
 }
