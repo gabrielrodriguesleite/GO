510070064e70371ee15200d77c842fb5718d4d0d
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-27 21:40:48 -0300

    [feat] by example | 66 Directories - listando arquivos em diretórios e navegando na árvore

--- Exercicios/GoByExample/66Directories/directories.go
+++ Exercicios/GoByExample/66Directories/directories.go
@@ -1,6 +1,9 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func check(e error) {
 	if e != nil {
@@ -32,4 +35,31 @@ func main() {
 	// MkdirAll similar ao comando mkdir -p cria a hierarquia de diretórios.
 	err = os.MkdirAll("subdir/parent/child", 0755)
 	check(err)
+
+	createEmptyFile("subdir/parent/file2")
+	createEmptyFile("subdir/parent/file3")
+	createEmptyFile("subdir/parent/child/file4")
+
+	c, err := os.ReadDir("subdir/parent")
+	check(err)
+
+	fmt.Println("Listing subdir/parent")
+	for _, entry := range c {
+		fmt.Println(" ", entry.Name(), entry.IsDir())
+	}
+
+	err = os.Chdir("subdir/parent/child")
+	check(err)
+
+	c, err = os.ReadDir(".")
+	check(err)
+
+	fmt.Println("Listing subdir/parent/child")
+	for _, entry := range c {
+		fmt.Println(" ", entry.Name(), entry.IsDir())
+	}
+
+	err = os.Chdir("../../../")
+	check(err)
+
 }
