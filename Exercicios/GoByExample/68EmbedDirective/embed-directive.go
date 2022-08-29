ef870fd8b21f6bf70186a92bf2b9320904cbec04
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-28 23:43:06 -0300

    [doc] go by example | 68 Embed Directive

---
+++ Exercicios/GoByExample/68EmbedDirective/embed-directive.go
@@ -0,0 +1,9 @@
+package main
+
+// go:embed é uma diretiva de compilador que permite um programa incluir
+// arquivos e diretórios no binário na hora da contrução.
+// Mais aqui: https://pkg.go.dev/embed
+// E aqui também: https://pkg.go.dev/cmd/compile#hdr-Compiler_Directives
+func main() {
+
+}
