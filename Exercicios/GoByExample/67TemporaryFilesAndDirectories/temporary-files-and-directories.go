f800ef64cf3517fbd8a368a0f7698fda32f07dd7
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-28 23:13:04 -0300

    [doc] go by example | 67 Temporary Files and Directoryes

---
+++ Exercicios/GoByExample/67TemporaryFilesAndDirectories/temporary-files-and-directories.go
@@ -0,0 +1,10 @@
+package main
+
+// Durante toda execução do programa, muitas vezes queremos
+// criar dados que não são necessários após a finalização do
+// programa. Arquivos e diretórios temporários são úteis para
+// este propósito desde que não poluam o sistema de arquivos
+// com o passar do tempo.
+func main() {
+
+}
