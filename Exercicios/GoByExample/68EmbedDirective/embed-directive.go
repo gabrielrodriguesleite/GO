8750905ea74ec8ca44bce34017111daf88d271d8
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-28 23:49:50 -0300

    [doc] go by example | 68 Embed Directive - embutindo um arquivo

--- Exercicios/GoByExample/68EmbedDirective/embed-directive.go
+++ Exercicios/GoByExample/68EmbedDirective/embed-directive.go
@@ -1,9 +1,19 @@
 package main
 
+import (
+	// Ao importar o pacote "embed" use algum identificador de exportação
+	// do pacote ou faça um importe em branco com "_"
+	_ "embed"
+)
+
 // "go:embed" é uma diretiva de compilador que permite um programa incluir
 // arquivos e diretórios no binário na hora da contrução.
 // Mais aqui: https://pkg.go.dev/embed
 // E aqui também: https://pkg.go.dev/cmd/compile#hdr-Compiler_Directives
+
+// A diretiva embed aceita caminhos relativos para o diretório contendo o
+// arquivo fonte. Esta diretiva irá imbutir o arquivo dentro da variável
+// do tipo string que segue a diretiva.
 
 // go:embed folder/single_file.txt
 var fileString string
