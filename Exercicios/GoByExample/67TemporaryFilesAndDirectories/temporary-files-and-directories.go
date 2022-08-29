c2ae73a81dffd03a11388dc6efb877c491dfe956
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-28 23:15:16 -0300

    [feat] go by example | 67 Temporary Files and Directoryes - Criando um arquivo temporário

--- Exercicios/GoByExample/67TemporaryFilesAndDirectories/temporary-files-and-directories.go
+++ Exercicios/GoByExample/67TemporaryFilesAndDirectories/temporary-files-and-directories.go
@@ -1,10 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // Durante toda execução do programa, muitas vezes queremos
 // criar dados que não são necessários após a finalização do
 // programa. Arquivos e diretórios temporários são úteis para
 // este propósito desde que não poluam o sistema de arquivos
 // com o passar do tempo.
-func main() {
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	f, err := os.CreateTemp("", "sample")
+	check(err)
+	fmt.Println("Temp file name:", f.Name())
 }
