cce4a2061bddb3b7b2d7b36af25e40c2ea7d93a8
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-30 23:16:33 -0300

    [feat] go by example | 71 Command Line Flags- flags de tipos diversos e flag que define o valor de uma variável por referência

--- Exercicios/GoByExample/71CommandLineFlags/command-line-flags.go
+++ Exercicios/GoByExample/71CommandLineFlags/command-line-flags.go
@@ -20,7 +20,17 @@ func main() {
 	// Abaixo vemos como usar este ponteiro.
 	wordPtr := flag.String("word", "foo", "a string")
 
+	numbPtr := flag.Int("numb", 42, "an int")
+	forkPtr := flag.Bool("fork", false, "a bool")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
+
 	flag.Parse()
 
 	fmt.Println("word:", *wordPtr)
+	fmt.Println("numb:", *numbPtr)
+	fmt.Println("fork:", *forkPtr)
+	fmt.Println("svar:", svar)
+	fmt.Println("tail:", flag.Args())
 }
