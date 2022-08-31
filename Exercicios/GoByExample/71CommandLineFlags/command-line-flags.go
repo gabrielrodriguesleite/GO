f5d624b0168ec5ec328119b6b346d2b25c26c724
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-30 23:05:08 -0300

    [doc] go by example | 71 Command Line Flags- wiki

--- Exercicios/GoByExample/71CommandLineFlags/command-line-flags.go
+++ Exercicios/GoByExample/71CommandLineFlags/command-line-flags.go
@@ -3,6 +3,8 @@ package main
 // Flags de linha de comando são uma forma fácil de especificar opções
 // para programas de linha de comando. Por exemplo, em "wc -l" o "-l" é
 // uma flag de linha de comando.
+// Mais em: https://en.wikipedia.org/wiki/Command-line_interface#Command-line_option
+
 func main() {
 
 }
