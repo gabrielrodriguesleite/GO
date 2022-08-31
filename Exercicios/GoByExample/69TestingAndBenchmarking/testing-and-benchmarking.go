f53a08c4e4030b3a1b5452bd89ddc2121293202f
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-30 22:07:17 -0300

    [doc] go by example | 69 Testing and Benchmarking

---
+++ Exercicios/GoByExample/69TestingAndBenchmarking/testing-and-benchmarking.go
@@ -0,0 +1,9 @@
+package main
+
+// Teste unitários são uma importante parte dos princípios de programação em Go.
+// O pacote de testes fornece as ferramentas necessárias para escrever testes
+// unitários. O comando "go test" roda os testes.
+
+func main() {
+
+}
