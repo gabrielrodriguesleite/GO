f7a63ee99aaaefe31a2d9963cbf52d3a4fe97bdc
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-30 23:04:19 -0300

    [doc] go by example | 71 Command Line Flags

---
+++ Exercicios/GoByExample/71CommandLineFlags/command-line-flags.go
@@ -0,0 +1,8 @@
+package main
+
+// Flags de linha de comando são uma forma fácil de especificar opções
+// para programas de linha de comando. Por exemplo, em "wc -l" o "-l" é
+// uma flag de linha de comando.
+func main() {
+
+}
