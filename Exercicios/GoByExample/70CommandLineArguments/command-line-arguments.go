f82fa9ecf53701ce0aaa453f89051e0b812d187b
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-30 22:49:27 -0300

    [doc] go by example | 70 Command Line Arguments - exemplo comum

---
+++ Exercicios/GoByExample/70CommandLineArguments/command-line-arguments.go
@@ -0,0 +1,9 @@
+package main
+
+// Argumentos de linha de comando são meios comuns de parametrizar a
+// execução de programas. Por exemplo "go run hello.go" usa "run" e
+// "hello.go" como argumentos para o programa "go".
+
+func main() {
+
+}
