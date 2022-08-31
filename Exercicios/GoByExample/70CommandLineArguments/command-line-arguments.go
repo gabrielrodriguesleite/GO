dd4fc46d0d5da51dae892d604224b4e876418a83
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-30 22:50:16 -0300

    [doc] go by example | 70 Command Line Arguments - wiki sobre argumentos na linha de comando

--- Exercicios/GoByExample/70CommandLineArguments/command-line-arguments.go
+++ Exercicios/GoByExample/70CommandLineArguments/command-line-arguments.go
@@ -3,6 +3,7 @@ package main
 // Argumentos de linha de comando são meios comuns de parametrizar a
 // execução de programas. Por exemplo "go run hello.go" usa "run" e
 // "hello.go" como argumentos para o programa "go".
+// Mais em: https://en.wikipedia.org/wiki/Command-line_interface#Arguments
 
 func main() {
 
