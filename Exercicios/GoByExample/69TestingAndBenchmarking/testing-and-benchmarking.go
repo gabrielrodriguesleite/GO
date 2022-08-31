8b849cafecf9d978acc5be8d112cccf461993c83
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-30 22:46:14 -0300

    [doc] go by example | 69 Testing and Benchmarking - Saída esperada adicionada

--- Exercicios/GoByExample/69TestingAndBenchmarking/testing-and-benchmarking.go
+++ Exercicios/GoByExample/69TestingAndBenchmarking/testing-and-benchmarking.go
@@ -75,3 +75,42 @@ func BenchmarkIntMin(b *testing.B) {
 		IntMin(1, 2)
 	}
 }
+
+/* SAÍDA ESPERADA PARA ESSE CÓDIGO:
+$ go test -v
+== RUN   TestIntMinBasic
+--- PASS: TestIntMinBasic (0.00s)
+=== RUN   TestIntMinTableDriven
+=== RUN   TestIntMinTableDriven/0,1
+=== RUN   TestIntMinTableDriven/1,0
+=== RUN   TestIntMinTableDriven/2,-2
+=== RUN   TestIntMinTableDriven/0,-1
+=== RUN   TestIntMinTableDriven/-1,0
+--- PASS: TestIntMinTableDriven (0.00s)
+    --- PASS: TestIntMinTableDriven/0,1 (0.00s)
+    --- PASS: TestIntMinTableDriven/1,0 (0.00s)
+    --- PASS: TestIntMinTableDriven/2,-2 (0.00s)
+    --- PASS: TestIntMinTableDriven/0,-1 (0.00s)
+    --- PASS: TestIntMinTableDriven/-1,0 (0.00s)
+PASS
+ok      examples/testing-and-benchmarking    0.023s
+
+$ go test -bench=.
+goos: darwin
+goarch: arm64
+pkg: examples/testing
+BenchmarkIntMin-8 1000000000 0.3136 ns/op
+PASS
+ok      examples/testing-and-benchmarking    0.351s
+
+*/
+
+// go test -v // roda todos os testes no projeto atual no modo verboso.
+
+// go test -bench=. // Rota todos os benchmarks no projeto atual.
+// Todos os tests são executados antes dos benchmarks. A flag bench
+// Filtra as funções com nome benchmark com um regexp.
+
+/* AVISO
+A estrutura atual dos arquivos não permite executar estes testes.
+*/
