498b0c37e01c4cd001aa6f96caad907fbbe8d42b
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-01 13:51:08 -0300

    [feat] go by example | 76 Context - Simple http server handler

--- Exercicios/GoByExample/76Context/context.go
+++ Exercicios/GoByExample/76Context/context.go
@@ -1,9 +1,19 @@
 package main
 
+import "net/http"
+
 // No exemplo anterior foi implementado um simples server.
 // Servers são úteis para demonstrar o controle de cancelamento com
 // "context.Context". Um "Context" carrega deadlines, sinais de cancelamento
 // e outros valores no escopo de requisições que cruzam entre API e rotinas go.
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+}
+
 func main() {
+
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8090", nil)
 
 }
