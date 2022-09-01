c93d9571e304d1c3379cb874461f6be6497da6a4
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-01 13:55:58 -0300

    [feat] go by example | 76 Context - Simple http server handler - context timeout

--- Exercicios/GoByExample/76Context/context.go
+++ Exercicios/GoByExample/76Context/context.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 // No exemplo anterior foi implementado um simples server.
 // Servers são úteis para demonstrar o controle de cancelamento com
@@ -8,7 +12,19 @@ import "net/http"
 // e outros valores no escopo de requisições que cruzam entre API e rotinas go.
 
 func hello(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	fmt.Println("server: hello handler started")
+	defer fmt.Println("server: hello handler ended")
 
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintf(w, "hello\n")
+	case <-ctx.Done():
+		err := ctx.Err()
+		fmt.Println("server:", err)
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
 }
 
 func main() {
