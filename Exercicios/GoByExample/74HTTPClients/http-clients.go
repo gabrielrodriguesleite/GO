3f537d7aec673379abfbee4c15f4a419a7fef05a
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-31 23:35:32 -0300

    [doc] go by example | 74 HTTP Clients - cliente padão e corpo da resposta

--- Exercicios/GoByExample/74HTTPClients/http-clients.go
+++ Exercicios/GoByExample/74HTTPClients/http-clients.go
@@ -11,14 +11,19 @@ import (
 // Neste exemplo este pacote será usado para lançar uma simple requisição HTTP
 func main() {
 
+	// Emitir uma requisição HTTP GET para um server usando http.Get é um atalho
+	// conveniente para criar um http.Client e chamar o método Get.
+	// Isto utiliza o objeto http.DefaultClient que possui configurações padrões úteis.
 	res, err := http.Get("https://gobyexample.com")
 	if err != nil {
 		panic(err)
 	}
 	defer res.Body.Close()
 
+	// Imprimimos o código de estado da resposta.
 	fmt.Println("Response status:", res.Status)
 
+	// Em seguida imprimimos as 5 primeira linhas do corpo da resposta
 	scanner := bufio.NewScanner(res.Body)
 	for i := 0; scanner.Scan() && i < 5; i++ {
 		fmt.Println(scanner.Text())
