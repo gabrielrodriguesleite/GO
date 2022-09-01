3ec578b1ec0865e94ddd19b001a3cf3a87c6d9b7
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-31 23:08:32 -0300

    [doc] go by example | 73 Environment Variables - wiki

--- Exercicios/GoByExample/73EnvironmentVariables/environment-variables.go
+++ Exercicios/GoByExample/73EnvironmentVariables/environment-variables.go
@@ -2,7 +2,10 @@ package main
 
 // Variáveis de ambiente são mecanismos universais para transmitir
 // informações de configuração para programas Unix.
+// Mais em: https://en.wikipedia.org/wiki/Environment_variable
+// Também: https://www.12factor.net/config
 // Neste exemplo as variáveis de ambiente serão listadas, escritas e lidas.
+
 func main() {
 
 }
