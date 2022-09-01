0ac1ae0ac9c07dd71128be11619b206c56b5327e
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-31 22:41:58 -0300

    [doc] go by example | 72 Subcomandos para comandos da linha de comando

---
+++ Exercicios/GoByExample/72CommandLineSubcommands/command-line-subcommands.go
@@ -0,0 +1,11 @@
+package main
+
+// Algumas ferramentas de linha de comando como ferramentas de"go" ou "git" possuem
+// muitos _subcomandos_ cada um com suas próprias flags.
+// Por exemplo, "go build" e "go get" são dois subcomandos diferentes das
+// ferramentas de "go". O pacote flag permite definir de maneira fácil
+// subcomandos com suas próprias flags.
+
+func main() {
+
+}
