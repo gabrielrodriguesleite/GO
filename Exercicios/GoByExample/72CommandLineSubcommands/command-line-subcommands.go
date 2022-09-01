041269e1b38944bb2d9363b00c3eaa91fbf46918
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-31 22:45:32 -0300

    [doc] go by example | 72 Subcomandos - foo

--- Exercicios/GoByExample/72CommandLineSubcommands/command-line-subcommands.go
+++ Exercicios/GoByExample/72CommandLineSubcommands/command-line-subcommands.go
@@ -10,6 +10,8 @@ import "flag"
 
 func main() {
 
+	// Aqui são declarados subcomandos usando "NewFlagSet" e então são
+	// definidas flags específicas para este comando.
 	fooCmd := flag.NewFlagSet("foo", flag.ExitOnError)
 	fooEnable := fooCmd.Bool("enable", false, "enable")
 	fooName := fooCmd.String("name", "", "name")
