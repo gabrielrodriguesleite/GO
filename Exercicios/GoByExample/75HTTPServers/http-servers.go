e159ad1094d3380e66e34ff8e019b13fffa3ab3d
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-31 23:40:30 -0300

    [feat] go by example | 75 HTTP Servers - handler básico

--- Exercicios/GoByExample/75HTTPServers/http-servers.go
+++ Exercicios/GoByExample/75HTTPServers/http-servers.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // Escrever um servido http básico é uma tarefa fácil usando o pacote
 // net/http que vem com Go
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
 func main() {
 
 }
