6dbf99d2e09187508bf5b76d7daaf0309419ff2d
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-31 22:44:19 -0300

    [feat] go by example | 72 Subcomandos para comandos da linha de comando

--- Exercicios/GoByExample/72CommandLineSubcommands/command-line-subcommands.go
+++ Exercicios/GoByExample/72CommandLineSubcommands/command-line-subcommands.go
@@ -1,5 +1,7 @@
 package main
 
+import "flag"
+
 // Algumas ferramentas de linha de comando como ferramentas de"go" ou "git" possuem
 // muitos _subcomandos_ cada um com suas próprias flags.
 // Por exemplo, "go build" e "go get" são dois subcomandos diferentes das
@@ -7,5 +9,9 @@ package main
 // subcomandos com suas próprias flags.
 
 func main() {
+
+	fooCmd := flag.NewFlagSet("foo", flag.ExitOnError)
+	fooEnable := fooCmd.Bool("enable", false, "enable")
+	fooName := fooCmd.String("name", "", "name")
 
 }
