d048ee643fce47ea1b9f39b9dc12236a25431153
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-31 23:26:50 -0300

    [doc] go by example | 74 HTTP Clients

---
+++ Exercicios/GoByExample/74HTTPClients/http-clients.go
@@ -0,0 +1,8 @@
+package main
+
+// A biblioteca padrão de go vem com um suporte excelente para clientes e
+// servidores HTTP no pacote "net/http".
+// Neste exemplo este pacote será usado para lançar uma simple requisição HTTP
+func main() {
+
+}
