3452a3e80e58aa2cc6e866f705f33ff320412894
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-01 13:47:27 -0300

    [doc] go by example | 76 Context

---
+++ Exercicios/GoByExample/76Context/context.go
@@ -0,0 +1,9 @@
+package main
+
+// No exemplo anterior foi implementado um simples server.
+// Servers são úteis para demonstrar o controle de cancelamento com
+// "context.Context". Um "Context" carrega deadlines, sinais de cancelamento
+// e outros valores no escopo de requisições que cruzam entre API e rotinas go.
+func main() {
+
+}
