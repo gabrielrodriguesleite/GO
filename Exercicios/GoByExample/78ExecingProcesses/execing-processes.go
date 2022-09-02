d78400caffcabd30d367a6082c40ff9842b2396c
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-01 23:22:50 -0300

    [doc] go by example | 78 Execing Processes

---
+++ Exercicios/GoByExample/78ExecingProcesses/execing-processes.go
@@ -0,0 +1,10 @@
+package main
+
+// No exemplo anterior vimos como lançar processos externos.
+// Fazemos isso quando precisamos um processo externo acessível para o
+// processo go que está rodando. Mas as vezes apenas queremos substituir
+// o processo Go com outro processo (não necessariamente um Go).
+// Para isso Go possui uma implementação para a clássica função "exec".
+func main() {
+
+}
