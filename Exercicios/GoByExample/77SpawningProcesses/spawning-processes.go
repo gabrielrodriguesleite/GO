5d59270c10344a22485ffa7374fac6e040fa9200
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-01 22:45:30 -0300

    [feat] go by example | 77 Spawning Processes - utilizando pipe na entrada e na saída padrão

--- Exercicios/GoByExample/77SpawningProcesses/spawning-processes.go
+++ Exercicios/GoByExample/77SpawningProcesses/spawning-processes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os/exec"
 )
 
@@ -27,4 +28,20 @@ func main() {
 			panic(err)
 		}
 	}
+
+	grepCmd := exec.Command("grep", "hello")
+
+	grepIn, _ := grepCmd.StdinPipe()
+	grepOut, _ := grepCmd.StdoutPipe()
+
+	grepCmd.Start()
+
+	grepIn.Write([]byte("hello grep\ngoodbye grep"))
+	grepIn.Close()
+
+	grepBytes, _ := io.ReadAll(grepOut)
+	grepCmd.Wait()
+
+	fmt.Println("> grep hello")
+	fmt.Println(string(grepBytes))
 }
