d0ce9c80ff024a5377c1a11ec2adb8d130203602
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-02 14:05:48 -0300

    [doc] go by example | 79 Signals - Manejando sinais do sistema com canais Go

--- Exercicios/GoByExample/79Signals/signals.go
+++ Exercicios/GoByExample/79Signals/signals.go
@@ -1,5 +1,11 @@
 package main
 
+// As vezes precisamos que nossa aplicação reaja aos sinais do Unix.
+// Mais em: https://en.wikipedia.org/wiki/Unix_signal
+// Por exemplo, quando temos um serviço rodando e queremos que ele finalize
+// corretamente quando recebe um SIGTERM, ou uma ferramenta de linha de comando
+// que pare de processar a entrada ao receber um SIGINT.
+// Neste exemplo veremos como manejar sinais do sistema com canais Go.
 func main() {
 
 }
