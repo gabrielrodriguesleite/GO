e8492114aa595185f29cf231a2f998abcad51574
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-01 23:25:48 -0300

    [doc] go by example | 78 Execing Processes - simples chamada

--- Exercicios/GoByExample/78ExecingProcesses/execing-processes.go
+++ Exercicios/GoByExample/78ExecingProcesses/execing-processes.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
 // No exemplo anterior vimos como lançar processos externos.
 // Fazemos isso quando precisamos um processo externo acessível para o
 // processo go que está rodando. Mas as vezes apenas queremos substituir
@@ -7,4 +13,16 @@ package main
 // Para isso Go possui uma implementação para a clássica função "exec".
 func main() {
 
+	binary, lookErr := exec.LookPath("ls")
+	if lookErr != nil {
+		panic(lookErr)
+	}
+
+	args := []string{"ls", "-a", "-l", "-h"}
+	env := os.Environ()
+
+	execErr := syscall.Exec(binary, args, env)
+	if execErr != nil {
+		panic(execErr)
+	}
 }
