2629e68e6b62bf62890b92f0529973864132af26
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-01 22:36:59 -0300

    [feat] go by example | 77 Spawning Processes

--- Exercicios/GoByExample/77SpawningProcesses/spawning-processes.go
+++ Exercicios/GoByExample/77SpawningProcesses/spawning-processes.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"os/exec"
+)
+
 // As vezes é necessário lançar outros processos de dentro da aplicação Go.
 func main() {
 
+	dateCmd := exec.Command("date")
+	dateOut, err := dateCmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("> date")
+	fmt.Println(string(dateOut))
 }
