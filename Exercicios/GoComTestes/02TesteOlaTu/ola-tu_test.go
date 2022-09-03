e68b85c1a6dfb749991aa0db79d481eade566b70
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-02 23:34:24 -0300

    [feat] go com testes | 02 ola tu - TDD primeira saída esperada

--- Exercicios/GoComTestes/02TesteOlaTu/ola-tu_test.go
+++ Exercicios/GoComTestes/02TesteOlaTu/ola-tu_test.go
@@ -11,3 +11,16 @@ func TestOlaTu(t *testing.T) {
 		t.ErrorF("Resultado: '%s'\nEsperado: '%s'", resultado, esperado)
 	}
 }
+
+// TDD RESULTADO 01:
+
+// Teste implementado:
+// R: Fail = função undefined
+
+/* go test 02TesteOlaTu/*
+# command-line-arguments [command-line-arguments.test]
+02TesteOlaTu/ola-tu_test.go:7:15: undefined: OlaTu
+02TesteOlaTu/ola-tu_test.go:11:5: t.ErrorF undefined (type *testing.T has no field or method ErrorF)
+FAIL    command-line-arguments [build failed]
+FAIL
+*/
