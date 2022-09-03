04f6683dd5747ddf60fa084b3f72a041b1bbe54b
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-02 21:45:20 -0300

    [feat] go by example | 79 Signals - implementando um "aguarde o Ctrl+C"

--- Exercicios/GoByExample/79Signals/signals.go
+++ Exercicios/GoByExample/79Signals/signals.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 // As vezes precisamos que nossa aplicação reaja aos sinais do Unix.
 // Mais em: https://en.wikipedia.org/wiki/Unix_signal
 // Por exemplo, quando temos um serviço rodando e queremos que ele finalize
@@ -8,4 +15,17 @@ package main
 // Neste exemplo veremos como manejar sinais do sistema com canais Go.
 func main() {
 
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	done := make(chan bool, 1)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println("\nSinal:", sig)
+		done <- true
+	}()
+
+	fmt.Println("Aguardando sinal...")
+	<-done
+	fmt.Println("Sinal recebido\nSaindo.")
 }
