edbf0fe079071a1a89c3a6e48367e05a256da9d6
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-02 23:19:41 -0300

    [feat] go com testes | 01 ola mundo - implementação do teste

--- Exercicios/GoComTestes/01TesteOlaMundo/ola_test.go
+++ Exercicios/GoComTestes/01TesteOlaMundo/ola_test.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "testing"
 
+func TestOla(t *testing.T) {
+	resultado := Ola()
+	esperado := "Olá, mundo"
+
+	if resultado != esperado {
+		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	}
 }
