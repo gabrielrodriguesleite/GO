8e42b9c0ddb07a3aae98a40872a27e96dcc1dca9
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-02 22:05:12 -0300

    [doc] go by example | 80 Exit - explicando defer com os.Exit

--- Exercicios/GoByExample/80Exit/exit.go
+++ Exercicios/GoByExample/80Exit/exit.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // Use "os.Exit" para sair imediatamente com um dado estado.
 
 func main() {
+
+	// "defer"s não vão ser executados quando sair por "os.Exit", então
+	// este "fmt.Println" nunca será chamado.
+	defer fmt.Println("!")
+
+	// Sair com estado 3
+	os.Exit(3)
 
 }
