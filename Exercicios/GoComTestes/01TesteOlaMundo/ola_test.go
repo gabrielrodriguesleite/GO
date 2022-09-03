897d7a6072a1043e7342fc3a2c2f1b62c528d6a7
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-02 23:20:57 -0300

    [doc] go com testes | 01 ola mundo - exemplo de saída de uma falha

--- Exercicios/GoComTestes/01TesteOlaMundo/ola_test.go
+++ Exercicios/GoComTestes/01TesteOlaMundo/ola_test.go
@@ -10,3 +10,12 @@ func TestOla(t *testing.T) {
 		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
 	}
 }
+
+// UM CÓDIGO COM FALHA IRIA IMPRIMIR ALGO ASSIM:
+/* go test 01TesteOlaMundo/*
+--- FAIL: TestOla (0.00s)
+    ola_test.go:10: resultado 'Olá, mundo', esperado 'Olá, mndo'
+FAIL
+FAIL    command-line-arguments  0.003s
+FAIL
+*/
