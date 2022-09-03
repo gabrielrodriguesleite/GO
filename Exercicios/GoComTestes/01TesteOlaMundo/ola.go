d955094e38a980adfe64737d9adf4a87e6cf0dda
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-02 23:14:21 -0300

    [feat] go com testes | 01 ola mundo - implementação com dominio e regra de negócio separados

--- Exercicios/GoComTestes/01TesteOlaMundo/ola.go
+++ Exercicios/GoComTestes/01TesteOlaMundo/ola.go
@@ -5,6 +5,11 @@ import "fmt"
 // Para testar é bom serpara o "domínio"(regras de negócio) do resto do mundo(efeitos colaterais).
 // A função fmt.Println é um efeito colateral(está imprimindo o valor na saída padrão do terminal).
 // A string que estamos usando dentro dela é o domínio.
+
+func Ola() string {
+	return "Olá, mundo"
+}
+
 func main() {
-	fmt.Println("Olá, mundo")
+	fmt.Println(Ola())
 }
