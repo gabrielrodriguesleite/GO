6a8b84a364f6a88d79a979b6dab496ac477ad317
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-03 21:42:12 -0300

    [test] go com testes | 02 - teste refatorado para incluir vários casos de teste

--- Exercicios/GoComTestes/02TesteOlaTu/ola-tu_test.go
+++ Exercicios/GoComTestes/02TesteOlaTu/ola-tu_test.go
@@ -4,32 +4,35 @@ import "testing"
 
 func TestOlaTu(t *testing.T) {
 
-	resultado := OlaTu("Gabriel")
-	esperado := "Olá, Gabriel"
+	// TDD RESULTADO 01:
 
-	if resultado != esperado {
-		t.Errorf("Resultado: '%s'\nEsperado: '%s'", resultado, esperado)
-	}
+	// Teste implementado:
+	// R: Fail = função undefined
+
+	/* go test 02TesteOlaTu/*
+	# command-line-arguments [command-line-arguments.test]
+	02TesteOlaTu/ola-tu_test.go:7:15: undefined: OlaTu
+	02TesteOlaTu/ola-tu_test.go:11:5: t.ErrorF undefined (type *testing.T has no field or method ErrorF)
+	FAIL    command-line-arguments [build failed]
+	FAIL
+	*/
+
+	// EXEMPLO DE ERRO QUANDO É PASSADO UM PARAMETRO A MAIS:
+	/* go test 02TesteOlaTu/*
+	# command-line-arguments [command-line-arguments.test]
+	02TesteOlaTu/ola-tu_test.go:7:32: too many arguments in call to OlaTu
+					have (string, string)
+					want (string)
+	FAIL    command-line-arguments [build failed]
+	FAIL*/
+	t.Run("Teste se diz olá para as pessoas", func(t *testing.T) {
+
+		resultado := OlaTu("Gabriel")
+		esperado := "Olá, Gabriel"
+
+		if resultado != esperado {
+			t.Errorf("Resultado: '%s'\nEsperado: '%s'", resultado, esperado)
+		}
+	})
+
 }
-
-// TDD RESULTADO 01:
-
-// Teste implementado:
-// R: Fail = função undefined
-
-/* go test 02TesteOlaTu/*
-# command-line-arguments [command-line-arguments.test]
-02TesteOlaTu/ola-tu_test.go:7:15: undefined: OlaTu
-02TesteOlaTu/ola-tu_test.go:11:5: t.ErrorF undefined (type *testing.T has no field or method ErrorF)
-FAIL    command-line-arguments [build failed]
-FAIL
-*/
-
-// EXEMPLO DE ERRO QUANDO É PASSADO UM PARAMETRO A MAIS:
-/* go test 02TesteOlaTu/*
-# command-line-arguments [command-line-arguments.test]
-02TesteOlaTu/ola-tu_test.go:7:32: too many arguments in call to OlaTu
-        have (string, string)
-        want (string)
-FAIL    command-line-arguments [build failed]
-FAIL*/
