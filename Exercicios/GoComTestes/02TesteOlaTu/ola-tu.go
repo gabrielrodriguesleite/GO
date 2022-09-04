910151d5a7f10534fa04fec41ce936cab2a8fe20
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-03 21:35:26 -0300

    [doc] go com testes | 02 - constantes melhorando a performance

--- Exercicios/GoComTestes/02TesteOlaTu/ola-tu.go
+++ Exercicios/GoComTestes/02TesteOlaTu/ola-tu.go
@@ -2,6 +2,9 @@ package main
 
 const prefixoOlaPortugues = "Olá, "
 
+// Constantes melhoram a performance da aplicação por evitar de
+// criar uma string "Olá, " toda vez que "OlaTu" é chamado.
+
 func OlaTu(nome string) string {
 	return prefixoOlaPortugues + nome
 }
