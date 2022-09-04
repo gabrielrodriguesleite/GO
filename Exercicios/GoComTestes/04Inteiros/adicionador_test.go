40030ecb51e8217c82a0a9678fc1d044ed5c991d
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-03 23:15:02 -0300

    [test] 04 Inteiros | TDD teste implementa novos casos

--- Exercicios/GoComTestes/04Inteiros/adicionador_test.go
+++ Exercicios/GoComTestes/04Inteiros/adicionador_test.go
@@ -3,10 +3,22 @@ package inteiros
 import "testing"
 
 func TestAdicionador(t *testing.T) {
-	soma := Adiciona(2, 2)
-	esperado := 4
 
-	if soma != esperado {
-		t.Errorf("Esperado: %d, resultado '%d'", esperado, soma)
-	}
+	t.Run("Testa se Adiciona(2, 2) é 4", func(t *testing.T) {
+		soma := Adiciona(2, 2)
+		esperado := 4
+
+		if soma != esperado {
+			t.Errorf("Esperado: %d, resultado '%d'", esperado, soma)
+		}
+	})
+
+	t.Run("Testa se Adiciona(4, 5) é 9", func(t *testing.T) {
+		soma := Adiciona(4, 5)
+		esperado := 9
+
+		if soma != esperado {
+			t.Errorf("Esperado: %d, resultado '%d'", esperado, soma)
+		}
+	})
 }
