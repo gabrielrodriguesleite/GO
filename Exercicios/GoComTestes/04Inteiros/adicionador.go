358ab7f45ad8d88b7b834449e66d756c211fd362
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-03 23:07:53 -0300

    [doc] 04 Inteiros | TDD verificar o erro na saída

--- Exercicios/GoComTestes/04Inteiros/adicionador.go
+++ Exercicios/GoComTestes/04Inteiros/adicionador.go
@@ -3,3 +3,13 @@ package inteiros
 func Adiciona(a, b int) int {
 	return 0
 }
+
+// VERIFICAR O ERRO NA SAÍDA DO TESTE
+
+/* go test 04Inteiros/*
+--- FAIL: TestAdicionador (0.00s)
+    adicionador_test.go:10: Esperado: 4, resultado '0'
+FAIL
+FAIL    command-line-arguments  0.003s
+FAIL
+*/
