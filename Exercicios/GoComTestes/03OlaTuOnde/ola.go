ac07e3579ba3604685b6be748436f20ab08c6d35
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-03 22:23:02 -0300

    [feat] 03 Ola tu onde - renomeados para fazer sentido

--- Exercicios/GoComTestes/03OlaTuOnde/ola.go
+++ Exercicios/GoComTestes/03OlaTuOnde/ola.go
@@ -5,7 +5,7 @@ const prefixoOlaPortugues = "Olá, "
 // Constantes melhoram a performance da aplicação por evitar de
 // criar uma string "Olá, " toda vez que "OlaTu" é chamado.
 
-func OlaTu(nome string) string {
+func Ola(nome string) string {
 	if nome == "" {
 		nome = "mundo"
 	}
