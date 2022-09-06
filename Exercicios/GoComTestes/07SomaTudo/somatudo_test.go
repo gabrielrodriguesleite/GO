6920f0d130c1fdada3fe73c767aa352bfa493ef2
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-05 23:16:39 -0300

    [test] 07 SomaTudo - TDD teste slice com reflect.DeepEqual

--- Exercicios/GoComTestes/07SomaTudo/somatudo_test.go
+++ Exercicios/GoComTestes/07SomaTudo/somatudo_test.go
@@ -1,13 +1,15 @@
 package somatudo
 
 import "testing"
+import "reflect"
 
 func TestSomaTudo(t *testing.T) {
 	resultado := SomaTudo([]int{1, 2}, []int{0, 9})
 	esperado := []int{3, 9}
 
-	// Este teste falha por que go não permite comparar slice com algo além de nil
-	if resultado != esperado {
+	// Este teste falha por que go não permite comparar slice com algo além de nil.
+	// if resultado != esperado {
+	if !reflect.DeepEqual(resultado, esperado) {
 		t.Errorf("Resultado: '%v', esperado '%v'", resultado, esperado)
 	}
 }
