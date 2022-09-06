eb2a6257e5fc9dfafd05f45bd61c881cfa8f2f41
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-05 22:48:25 -0300

    [test] 06 Soma - TDD Teste modificado fix do teste 1

--- Exercicios/GoComTestes/06Soma/soma_test.go
+++ Exercicios/GoComTestes/06Soma/soma_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 	t.Run("Coleção de 5 números", func(t *testing.T) {
-		numeros := [5]int{1, 2, 3, 4, 5}
+		numeros := []int{1, 2, 3, 4, 5}
 		resultado := Soma(numeros)
 		esperado := 15
 		if esperado != resultado {
