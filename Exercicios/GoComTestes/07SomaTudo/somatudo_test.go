0d57aa7802431a8b30c1d4fd49fd5cbb0972eb73
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-05 23:11:54 -0300

    [doc] 07 SomaTudo - TDD teste slice com nil

--- Exercicios/GoComTestes/07SomaTudo/somatudo_test.go
+++ Exercicios/GoComTestes/07SomaTudo/somatudo_test.go
@@ -6,6 +6,7 @@ func TestSomaTudo(t *testing.T) {
 	resultado := SomaTudo([]int{1, 2}, []int{0, 9})
 	esperado := []int{3, 9}
 
+	// Este teste falha por que go não permite comparar slice com algo além de nil
 	if resultado != esperado {
 		t.Errorf("Resultado: '%v', esperado '%v'", resultado, esperado)
 	}
