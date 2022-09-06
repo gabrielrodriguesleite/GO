f7062899f3a04942832e11b71f546f5d22c14a0d
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-05 23:17:14 -0300

    [doc] 07 SomaTudo - TDD reflect.DeepEqual não assegura tipagem

--- Exercicios/GoComTestes/07SomaTudo/somatudo_test.go
+++ Exercicios/GoComTestes/07SomaTudo/somatudo_test.go
@@ -5,9 +5,11 @@ import "reflect"
 
 func TestSomaTudo(t *testing.T) {
 	resultado := SomaTudo([]int{1, 2}, []int{0, 9})
+	// esperado := "joao"
 	esperado := []int{3, 9}
 
 	// Este teste falha por que go não permite comparar slice com algo além de nil.
+	// A solução é usar DeepEqual do pacote reflect porém ele não assegura tipos.
 	// if resultado != esperado {
 	if !reflect.DeepEqual(resultado, esperado) {
 		t.Errorf("Resultado: '%v', esperado '%v'", resultado, esperado)
