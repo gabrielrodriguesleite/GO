e9bb48fceb8548371f18fbd755d438ed7aa52022
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-05 23:39:17 -0300

    [feat] 08 SomaTodoResto - TDD função implementada passa no teste

--- Exercicios/GoComTestes/08SomaTodoResto/soma-todo-resto.go
+++ Exercicios/GoComTestes/08SomaTodoResto/soma-todo-resto.go
@@ -1,5 +1,16 @@
 package somatodoresto
 
+func Soma(a []int) (s int) {
+	for _, n := range a {
+		s += n
+	}
+	return
+}
+
 func SomaTodoResto(arr ...[]int) (res []int) {
+	for _, num := range arr {
+		resto := num[1:]
+		res = append(res, Soma(resto))
+	}
 	return
 }
