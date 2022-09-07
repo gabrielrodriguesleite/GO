b7153412247b28a11561391199c3b97c0e4d6aa8
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-06 22:24:11 -0300

    [test] 09 Estruturas Metodos Interfaces - TDD teste para segunda função implementado

--- Exercicios/GoComTestes/09EstruturasMetodosInterfaces/formas_test.go
+++ Exercicios/GoComTestes/09EstruturasMetodosInterfaces/formas_test.go
@@ -12,3 +12,12 @@ func TestPerimetro(t *testing.T) {
 		t.Errorf("Resultado: '%.2f' esperado: '%.2f'", res, esp)
 	}
 }
+
+func TestArea(t *testing.T) {
+	res := Area(12.0, 6.0)
+	esp := 72.0
+
+	if res != esp {
+		t.Errorf("Resultado: '%.2f' esperado: '%.2f'", res, esp)
+	}
+}
