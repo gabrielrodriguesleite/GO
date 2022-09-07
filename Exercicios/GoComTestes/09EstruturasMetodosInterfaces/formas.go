1099fe7b80722e456c63aa691ec57a0359a86376
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-06 22:43:12 -0300

    [feat] 09 Estruturas ... - Implementação dos métodos Area

--- Exercicios/GoComTestes/09EstruturasMetodosInterfaces/formas.go
+++ Exercicios/GoComTestes/09EstruturasMetodosInterfaces/formas.go
@@ -1,5 +1,7 @@
 package formas
 
+import "math"
+
 type Retangulo struct {
 	Largura float64
 	Altura  float64
@@ -13,6 +15,10 @@ func Perimetro(ret Retangulo) (per float64) {
 	return 2 * (ret.Largura + ret.Altura)
 }
 
-func Area(ret Retangulo) (area float64) {
-	return ret.Largura * ret.Altura
+func (r Retangulo) Area() (area float64) {
+	return r.Largura * r.Altura
+}
+
+func (c Circulo) Area() (area float64) {
+	return math.Pi * c.Raio * c.Raio
 }
