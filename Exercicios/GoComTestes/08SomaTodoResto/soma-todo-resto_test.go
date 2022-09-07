26d6f600a89045f2bceefde1c9b11c9952bef8a5
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-06 22:02:43 -0300

    [test] 08 SomaTodoResto - Teste helper implementado

--- Exercicios/GoComTestes/08SomaTodoResto/soma-todo-resto_test.go
+++ Exercicios/GoComTestes/08SomaTodoResto/soma-todo-resto_test.go
@@ -4,14 +4,20 @@ import "testing"
 import "reflect"
 
 func TestSomaTodoResto(t *testing.T) {
+
+	verificaSomas := func(t *testing.T, res, esp []int) {
+		t.Helper()
+		if !reflect.DeepEqual(res, esp) {
+			t.Errorf("Resultado: '%v', esperado: '%v", res, esp)
+		}
+	}
+
 	t.Run("Soma alguns slices.", func(t *testing.T) {
 
 		res := SomaTodoResto([]int{1, 2}, []int{0, 9})
 		esp := []int{2, 9}
 
-		if !reflect.DeepEqual(res, esp) {
-			t.Errorf("Resultado: '%v', esperado: '%v", res, esp)
-		}
+		verificaSomas(t, res, esp)
 	})
 
 	t.Run("Soma slices vazios de forma segura.", func(t *testing.T) {
@@ -19,9 +25,7 @@ func TestSomaTodoResto(t *testing.T) {
 		res := SomaTodoResto([]int{}, []int{3, 4, 5})
 		esp := []int{0, 9}
 
-		if !reflect.DeepEqual(res, esp) {
-			t.Errorf("Resultado: '%v', esperado: '%v", res, esp)
-		}
+		verificaSomas(t, res, esp)
 	})
 
 }
