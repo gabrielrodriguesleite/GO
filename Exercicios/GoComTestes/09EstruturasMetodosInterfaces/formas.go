1b5ec2f21f3be3a22432520e0b1a542cf58bb7f8
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-06 22:31:53 -0300

    [feat] 09 Estruturas ... - Implementação da função Area para um Retangulo

--- Exercicios/GoComTestes/09EstruturasMetodosInterfaces/formas.go
+++ Exercicios/GoComTestes/09EstruturasMetodosInterfaces/formas.go
@@ -9,6 +9,6 @@ func Perimetro(ret Retangulo) (per float64) {
 	return 2 * (ret.Largura + ret.Altura)
 }
 
-func Area(lar float64, alt float64) (area float64) {
-	return lar * alt
+func Area(ret Retangulo) (area float64) {
+	return ret.Largura * ret.Altura
 }
