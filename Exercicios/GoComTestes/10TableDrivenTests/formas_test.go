b569dcbbeec9fd2222d5c978ee7aad9bedba7524
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-06 23:12:19 -0300

    [test] 10 Table Driven Test - teste implementado

---
+++ Exercicios/GoComTestes/10TableDrivenTests/formas_test.go
@@ -0,0 +1,22 @@
+package formas
+
+import (
+	"testing"
+)
+
+func TestArea(t *testing.T) {
+	testesArea := []struct {
+		forma    Forma
+		esperado float64
+	}{
+		{Retangulo{12, 6}, 72.0},
+		{Circulo{10}, 314.1592653589793},
+	}
+
+	for _, tt := range testesArea {
+		resultado := tt.forma.Area()
+		if resultado != tt.esperado {
+			t.Errorf("Resultado: '%.2f' esperado '%.2f'", resultado, tt.esperado)
+		}
+	}
+}
