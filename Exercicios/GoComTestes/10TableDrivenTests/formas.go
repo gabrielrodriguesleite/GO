95a4f5eb7be738a3d10fc709b4cca4629926d2d4
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-06 23:12:54 -0300

    [feat] 10 Table Driven Test - código com interface estruturas e métodos reaproveitado

---
+++ Exercicios/GoComTestes/10TableDrivenTests/formas.go
@@ -0,0 +1,28 @@
+package formas
+
+import "math"
+
+type Forma interface {
+	Area() float64
+}
+
+type Retangulo struct {
+	Largura float64
+	Altura  float64
+}
+
+type Circulo struct {
+	Raio float64
+}
+
+func Perimetro(ret Retangulo) (per float64) {
+	return 2 * (ret.Largura + ret.Altura)
+}
+
+func (r Retangulo) Area() (area float64) {
+	return r.Largura * r.Altura
+}
+
+func (c Circulo) Area() (area float64) {
+	return math.Pi * c.Raio * c.Raio
+}
