fabb3719c36ae7142b81deb1a7e510f994ebe397
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-06 21:58:19 -0300

    [test] 08 SomaTodoResto - novo caso para slices vazios

--- Exercicios/GoComTestes/08SomaTodoResto/soma-todo-resto_test.go
+++ Exercicios/GoComTestes/08SomaTodoResto/soma-todo-resto_test.go
@@ -4,10 +4,20 @@ import "testing"
 import "reflect"
 
 func TestSomaTodoResto(t *testing.T) {
-	t.Run("Soma alguns slices", func(t *testing.T) {
+	t.Run("Soma alguns slices.", func(t *testing.T) {
 
 		res := SomaTodoResto([]int{1, 2}, []int{0, 9})
 		esp := []int{2, 9}
+
+		if !reflect.DeepEqual(res, esp) {
+			t.Errorf("Resultado: '%v', esperado: '%v", res, esp)
+		}
+	})
+
+	t.Run("Soma slices vazios de forma segura.", func(t *testing.T) {
+
+		res := SomaTodoResto([]int{}, []int{3, 4, 5})
+		esp := []int{0, 9}
 
 		if !reflect.DeepEqual(res, esp) {
 			t.Errorf("Resultado: '%v', esperado: '%v", res, esp)
