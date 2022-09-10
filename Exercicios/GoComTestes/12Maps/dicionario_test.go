6494461821db72ecc0f05cb0d493e5df4b6c8bc9
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-09 22:25:27 -0300

    [test] 12 Maps - teste para o método Busca com retorno de erro

--- Exercicios/GoComTestes/12Maps/dicionario_test.go
+++ Exercicios/GoComTestes/12Maps/dicionario_test.go
@@ -6,11 +6,17 @@ func TestBusca(t *testing.T) {
 	dicionario := Dicionario{"teste": "isso é apenas um teste"}
 
 	t.Run("palavra conhecida", func(t *testing.T) {
-		resultado := dicionario.Busca("teste")
+		resultado, _ := dicionario.Busca("teste")
 		esperado := "isso é apenas um teste"
 		comparaString(t, resultado, esperado)
 	})
 
+	t.Run("palavra desconhecida", func(t *testing.T) {
+		_, err := dicionario.Busca("desconhecida")
+		if err == nil {
+			t.Fatal("é esperado que um erro seja obtido")
+		}
+	})
 }
 
 func comparaString(t *testing.T, resultado, esperado string) {
