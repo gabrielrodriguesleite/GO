263d6e66f33caeb84f71a5a7c0ca61f3533e66ea
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-09 22:28:28 -0300

    [feat] 12 Maps - implementado método Busca para passar no novo teste

--- Exercicios/GoComTestes/12Maps/dicionario.go
+++ Exercicios/GoComTestes/12Maps/dicionario.go
@@ -1,7 +1,13 @@
 package main
 
+import "errors"
+
 type Dicionario map[string]string
 
 func (d Dicionario) Busca(palavra string) (retorno string, erro error) {
-	return d[palavra], nil
+	definicao, existe := d[palavra]
+	if !existe {
+		return "", errors.New("não foi possível encontrar a palavra que você buscou")
+	}
+	return definicao, nil
 }
