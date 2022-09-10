10028262160d0cd2ac07a95053ec9227e921589a
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-09 22:45:08 -0300

    [doc] 12 Maps - map é um tipo de referência

--- Exercicios/GoComTestes/12Maps/dicionario.go
+++ Exercicios/GoComTestes/12Maps/dicionario.go
@@ -4,6 +4,9 @@ import "errors"
 
 var ErrNaoEncontrado = errors.New("não foi possível encontrar a palavra que você buscou")
 
+// Maps é um tipo de referência por isso podemos modificá-lo sem passar como ponteiro.
+// Maps usa uma estrutura de dados chamada tabela de dispersão ou mapa de hash.
+// Mais aqui: https://pt.wikipedia.org/wiki/Tabela_de_dispers%C3%A3o
 type Dicionario map[string]string
 
 func (d Dicionario) Busca(palavra string) (retorno string, erro error) {
