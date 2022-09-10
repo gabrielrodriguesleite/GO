7bf4384674d0e7672dfd0ab46ee289e705caebc3
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-09 22:31:26 -0300

    [feat] 12 Maps - variavel pra armazenar o erro

--- Exercicios/GoComTestes/12Maps/dicionario.go
+++ Exercicios/GoComTestes/12Maps/dicionario.go
@@ -2,12 +2,14 @@ package main
 
 import "errors"
 
+var ErrNaoEncontrado = errors.New("não foi possível encontrar a palavra que você buscou")
+
 type Dicionario map[string]string
 
 func (d Dicionario) Busca(palavra string) (retorno string, erro error) {
 	definicao, existe := d[palavra]
 	if !existe {
-		return "", errors.New("não foi possível encontrar a palavra que você buscou")
+		return "", ErrNaoEncontrado
 	}
 	return definicao, nil
 }
