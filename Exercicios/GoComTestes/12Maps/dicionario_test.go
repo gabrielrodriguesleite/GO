4ab39853fd746944049f7b7bba7f93f0a8b88a00
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-09 22:10:00 -0300

    [test] 12 Maps - teste implementado

---
+++ Exercicios/GoComTestes/12Maps/dicionario_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestBusca(t *testing.T) {
+	dicionario := map[string]string{"teste": "isso é apenas um teste"}
+	resultado := Busca(dicionario, "teste")
+	esperado := "isso é apenas um teste"
+	if resultado != esperado {
+		t.Errorf("Resultado: '%s', esperado '%s', dado: '%s'", resultado, esperado, "test")
+	}
+}
