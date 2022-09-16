1958841a68abee8353df74370d84e258f7242b7f
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-15 22:48:36 -0300

    [feat] go com testes = 14 Mocks = função implementada para "passar"

--- Exercicios/GoComTestes/14Mocks/contagem.go
+++ Exercicios/GoComTestes/14Mocks/contagem.go
@@ -1,6 +1,9 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+)
 
 /* DEFINIÇÃO DO PROBLEMA:
 Escrever um programa que conta regressivamente a partir de 3 e então
@@ -8,7 +11,7 @@ imprime "Vai!" (sem imprimir o zero)
 */
 
 func Contagem(saida *bytes.Buffer) {
-
+	fmt.Fprint(saida, "3")
 }
 
 func main() {
