c99b0283282c926d2e2a74f42ba716211609c86a
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-15 23:24:55 -0300

    [feat] go com testes = 14 Mocks = função utilizando a dependencia injetada permitindo o teste rodar rápidamente

--- Exercicios/GoComTestes/14Mocks/contagem.go
+++ Exercicios/GoComTestes/14Mocks/contagem.go
@@ -33,11 +33,11 @@ const inicioContagem = 3
 
 func Contagem(saida io.Writer, sleeper Sleeper) {
 	for i := inicioContagem; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(saida, i)
 	}
 
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(saida, ultimaPalavra)
 }
 
