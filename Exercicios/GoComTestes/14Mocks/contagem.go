905963e8149a1fcc6a25e3c3a25eec7c9cb6cf4a
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-15 23:08:01 -0300

    [doc] go com testes = 14 Mocks = comentario sobre injeção de dependencia neste exemplo

--- Exercicios/GoComTestes/14Mocks/contagem.go
+++ Exercicios/GoComTestes/14Mocks/contagem.go
@@ -12,6 +12,12 @@ Escrever um programa que conta regressivamente a partir de 3 e então
 imprime "Vai!" (sem imprimir o zero)
 */
 
+/*
+Este exemplo deixa claro que o sleep irá atrasar os testes.
+Neste caso é essencial usar a injeção de dependência pra
+contornar este problema.
+*/
+
 const ultimaPalavra = "Vai!"
 const inicioContagem = 3
 
