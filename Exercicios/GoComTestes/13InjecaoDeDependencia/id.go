8975117df3c14453c0dadf56c9b3066cec9d0e6d
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-15 21:56:18 -0300

    [feat] go com testes = 13 Injeção de Dependência = função comprovada.

--- Exercicios/GoComTestes/13InjecaoDeDependencia/id.go
+++ Exercicios/GoComTestes/13InjecaoDeDependencia/id.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
 )
 
 func Cumprimenta(escritor io.Writer, nome string) {
 	fmt.Fprintf(escritor, "Olá, %s", nome)
+}
+
+func main() {
+	Cumprimenta(os.Stdout, "Hello\n")
 }
