535a1b15b286585871fd94bd791188759bf207c6
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-15 22:47:47 -0300

    [feat] go com testes = 14 Mocks = parametro da função definido

--- Exercicios/GoComTestes/14Mocks/contagem.go
+++ Exercicios/GoComTestes/14Mocks/contagem.go
@@ -1,14 +1,16 @@
 package main
 
+import "bytes"
+
 /* DEFINIÇÃO DO PROBLEMA:
 Escrever um programa que conta regressivamente a partir de 3 e então
 imprime "Vai!" (sem imprimir o zero)
 */
 
-func Contagem() {
+func Contagem(saida *bytes.Buffer) {
 
 }
 
 func main() {
-	Contagem()
+	// Contagem()
 }
