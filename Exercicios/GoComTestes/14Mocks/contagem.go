6f366ec2827bdc96dfefbde11c068ec064261d71
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-15 22:17:59 -0300

    [feat] go com testes = 14 Mocks = função criada

--- Exercicios/GoComTestes/14Mocks/contagem.go
+++ Exercicios/GoComTestes/14Mocks/contagem.go
@@ -1,6 +1,14 @@
-package mocks
+package main
 
 /* DEFINIÇÃO DO PROBLEMA:
 Escrever um programa que conta regressivamente a partir de 3 e então
 imprime "Vai!" (sem imprimir o zero)
 */
+
+func Contagem() {
+
+}
+
+func main() {
+	Contagem()
+}
