3969cbe6f22558aaa03d4373fd97f8eb9f80c325
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-19 23:13:36 -0300

    [test] 15 Concorrencia - Teste implementado

--- Exercicios/GoComTestes/15Concorrencia/concorrencia_test.go
+++ Exercicios/GoComTestes/15Concorrencia/concorrencia_test.go
@@ -1,6 +1,9 @@
 package concorrencia
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func mockVerificadorWebsite(url string) bool {
 	if url == "waat://furhurterwe.geds" {
@@ -10,5 +13,21 @@ func mockVerificadorWebsite(url string) bool {
 }
 
 func TestVerificaWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
 
+	esperado := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	resultado := VerificadorWebsites(mockVerificadorWebsite, websites)
+
+	if !reflect.DeepEqual(esperado, resultado) {
+		t.Fatalf("esperado %v, resultado %v", esperado, resultado)
+	}
 }
