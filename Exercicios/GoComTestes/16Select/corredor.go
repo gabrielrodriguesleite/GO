908c80dddaa09b74a62e08a9dfd5190e0e6a95f7
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-20 18:38:09 -0300

    [test] go com testes -- 16 Select-- função implementadafeat

--- Exercicios/GoComTestes/16Select/corredor.go
+++ Exercicios/GoComTestes/16Select/corredor.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"net/http"
+	"time"
+)
+
 // Problemática:
 // Fazer uma função chamada Corredor que recebe duas URLs que
 // "competirão" entre si através de uma chamada HTTP GET onde a
@@ -16,6 +21,17 @@ package main
 
 */
 
-func Corredor(URLLenta, URLRapida string) (err string) {
-	return
+func Corredor(a, b string) (vencedor string) {
+	inicioA := time.Now()
+	http.Get(a)
+	ducacaoA := time.Since(inicioA)
+
+	inicioB := time.Now()
+	http.Get(b)
+	ducacaoB := time.Since(inicioB)
+
+	if ducacaoA < ducacaoB {
+		return a
+	}
+	return b
 }
