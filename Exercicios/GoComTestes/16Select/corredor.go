ff3922008a99a861ab0246dbc845a3b7ac473962
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-20 20:27:32 -0300

    [feat] go com testes -- 16 Select -- função Corredor virou uma função 'interface' para adimitir o teste com timeout configuravel

--- Exercicios/GoComTestes/16Select/corredor.go
+++ Exercicios/GoComTestes/16Select/corredor.go
@@ -22,7 +22,13 @@ import (
 
 */
 
-func Corredor(a, b string, tempoLimite time.Duration) (vencedor string, erro error) {
+var limiteDeDezSegundos = 10 * time.Second
+
+func Corredor(a, b string) (vencedor string, erro error) {
+	return Configuravel(a, b, limiteDeDezSegundos)
+}
+
+func Configuravel(a, b string, tempoLimite time.Duration) (vencedor string, erro error) {
 	select {
 	case <-ping(a):
 		return a, nil
