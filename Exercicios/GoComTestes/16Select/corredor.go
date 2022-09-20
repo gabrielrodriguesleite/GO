60ce60779d334b1c11fd181bc93a901f4a0fa28d
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-20 10:01:33 -0300

    [doc] go com testes -- 15 Select-- instruções do 'projeto'

---
+++ Exercicios/GoComTestes/16Select/corredor.go
@@ -0,0 +1,17 @@
+package main
+
+// Problemática:
+// Fazer uma função chamada Corredor que recebe duas URLs que
+// "competirão" entre si através de uma chamada HTTP GET onde a
+// primeira URL a responder será retornada.
+// Caso nenhuma responda dentro de 10 segundos a função deve
+// retornar uma erro.
+
+/*
+
++ net/http - chamadas http
++ net/http/httptest - testes http
++ goroutines - paralelismo
++ select - sincronia de processos
+
+*/
