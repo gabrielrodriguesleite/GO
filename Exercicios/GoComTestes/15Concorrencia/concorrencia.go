a07fc560238b7e57fa58b696f5f79740323bb517
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-19 23:05:11 -0300

    [doc] 15 Concorrencia - função de exemplo que verifica o estado de uma lista de URLs

--- Exercicios/GoComTestes/15Concorrencia/concorrencia.go
+++ Exercicios/GoComTestes/15Concorrencia/concorrencia.go
@@ -11,3 +11,8 @@ func VerificadorWebsites(vw VerificadorWebsite, urls []string) map[string]bool {
 
 	return resultados
 }
+
+// Retorna um map de cada URL verificado com um valor boleano verdadeiro para
+// uma resposta boa e falsa para uma resposta ruim.
+// Recebe um VerificadorWebsite como parâmetro que leva uma url e retorna um boleano.
+// Veja o teste →
