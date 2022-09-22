eaa58c8158968e6c6ff6aa530d50a18e14dd25d1
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-21 21:16:36 -0300

    [doc] go com testes -- 17 Reflexão -- TDD função comentada : desafio exposto

--- Exercicios/GoComTestes/17Reflexao/reflexao.go
+++ Exercicios/GoComTestes/17Reflexao/reflexao.go
@@ -1,5 +1,9 @@
 package main
 
-func percorre(interface{}, func(entrada string)) {
+// Desafio: escrever uma função percorre(x interface{}, fn func(entrada string))
+// que recebe uma struct x e chama fn para todos os campos string encontrados
+// dentro dela.
+
+func percorre(x interface{}, fn func(entrada string)) {
 
 }
