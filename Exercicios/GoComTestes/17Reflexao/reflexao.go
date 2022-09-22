04a988dd316eadc1816767612c559ced8a02a44a
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-21 21:19:19 -0300

    [doc] go com testes -- 17 Reflexão -- Reflexão explicada (por The Go Blog: Reflection)

--- Exercicios/GoComTestes/17Reflexao/reflexao.go
+++ Exercicios/GoComTestes/17Reflexao/reflexao.go
@@ -4,6 +4,13 @@ package main
 // que recebe uma struct x e chama fn para todos os campos string encontrados
 // dentro dela.
 
+// Nesse desafio será utilizado o conceito de reflection (reflexão).
+
+// A reflexão em computação é a habilidade de um programa examinar sua]
+// própria estrutura, particularmente através de tipos; é uma forma de
+// metaprogramação. Também é uma ótima fonte de confusão. -- The Go Blog: Reflection
+// fonte: https://blog.golang.org/laws-of-reflection
+
 func percorre(x interface{}, fn func(entrada string)) {
 
 }
