77f93f95871260e97be136097359856820ba443e
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-22 23:02:25 -0300

    [doc] go com testes -- 19 Context  -- Contexto e problemática sobre context

---
+++ Exercicios/GoComTestes/19Context/context.go
@@ -0,0 +1,12 @@
+package main
+
+// CONTEXTO
+
+/* O pacote context ajuda a gerenciar processos demorados
+de forma consistente dentro da aplicação.
+
+Por exemplo quando a aplicação está buscando um recurso e
+no meio recebe um sinal para desistir. Simplesmente abandonar
+a goroutine e deixá-la processando não é nem eficiente nem ágil.
+
+*/
