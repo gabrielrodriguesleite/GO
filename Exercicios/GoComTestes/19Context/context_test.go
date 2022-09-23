9acb46fa4605fff931e7f15d84cf85c6b0a2575d
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-22 23:11:19 -0300

    [test] go com testes -- 19 Context -- caso unico implementado

--- Exercicios/GoComTestes/19Context/context_test.go
+++ Exercicios/GoComTestes/19Context/context_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 type StubStore struct {
 	response string
@@ -11,5 +15,15 @@ func (s *StubStore) Fetch() string {
 }
 
 func TestHandler(t *testing.T) {
+	data := "Opa! Tchê"
+	svr := Server(&StubStore{data})
 
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	svr.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf(`resultado "%s", esperado "%s"`, response.Body.String(), data)
+	}
 }
