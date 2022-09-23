8ea315d4da78084e8cacf6d6815dbfdca3c190b7
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-22 22:05:00 -0300

    [doc] go com testes -- 18 Sincronismo -- problematica

--- Exercicios/GoComTestes/18Sync/sync_test.go
+++ Exercicios/GoComTestes/18Sync/sync_test.go
@@ -2,6 +2,11 @@ package main
 
 import "testing"
 
+// PROBLEMÁTICA: O objetivo é criar um contador seguro para concorrência.
+// Primeiramente será desenvolvido um contador não seguro para verificar
+// o funcionamento em um ambiente com apenas 1 linha de execução.
+// Então serão utilizados várias go routines para expor a falha e então
+// consertar.
 func TestContador(t *testing.T) {
 	t.Run("", func(t *testing.T) {
 
