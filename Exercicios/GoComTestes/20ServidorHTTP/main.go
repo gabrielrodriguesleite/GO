da954f28f6098c0805ab7a0358893f30f54f8c14
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-23 23:31:38 -0300

    [doc] go com testes -- 20 Servidor HTTP -- TDD HandlerFunc addapter

--- Exercicios/GoComTestes/20ServidorHTTP/main.go
+++ Exercicios/GoComTestes/20ServidorHTTP/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	tratador := http.HandlerFunc(ServidorJogador)
+	tratador := http.HandlerFunc(ServidorJogador) // HandlerFunc é um adaptador
+	// que permite usar funções comuns como tratadores.
 	log.Println("Escutando na porta 5000")
 	if err := http.ListenAndServe(":5000", tratador); err != nil {
 		log.Fatalf("não foi possível escutar na porta 5000 %v", err)
