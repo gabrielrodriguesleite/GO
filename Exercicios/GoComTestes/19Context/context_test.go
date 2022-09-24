65da600b4229d2c2b365a36075029379d0874494
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-23 22:16:04 -0300

    [test] go com testes -- 19 Context -- Teste atualizado pra spy usar o contexto

--- Exercicios/GoComTestes/19Context/context_test.go
+++ Exercicios/GoComTestes/19Context/context_test.go
@@ -13,8 +13,30 @@ type SpyStore struct {
 	t        *testing.T
 }
 
-func (s *SpyStore) Fetch(ctx context.Context) string {
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
 
+	go func() {
+		var result string
+		for _, c := range s.response {
+			select {
+			case <-ctx.Done():
+				s.t.Log("spy store foi cancelado")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(c)
+			}
+		}
+		data <- result
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
+	}
 }
 
 func (s *SpyStore) assertWasCancelled() {
