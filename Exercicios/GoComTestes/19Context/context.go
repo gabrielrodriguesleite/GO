b0d2925286f710a12c191ac6b1774c5cb05a7216
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-23 21:53:29 -0300

    [feat] go com testes -- 19 Context -- função passa de modo coerente - contexto de cancelamento

--- Exercicios/GoComTestes/19Context/context.go
+++ Exercicios/GoComTestes/19Context/context.go
@@ -18,8 +18,19 @@ a goroutine e deixá-la processando não é nem eficiente nem ágil.
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		store.Cancel()
-		fmt.Fprint(w, store.Fetch())
+		ctx := r.Context()
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
 	}
 }
 
