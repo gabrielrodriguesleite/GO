e5bfe84638648a7a325551b2c81fdead38741e63
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-23 22:10:16 -0300

    [doc] go com testes -- 19 Context -- função refatorada para utilizar o contexto com downstream

--- Exercicios/GoComTestes/19Context/context.go
+++ Exercicios/GoComTestes/19Context/context.go
@@ -18,7 +18,11 @@ a goroutine e deixá-la processando não é nem eficiente nem ágil.
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		// Será utilzado o context para que não seja necessário
+		// chamar uma função de cancelamento pra cada método.
+		// O handler será responsável em emitir o contexto à
+		// Store em cascata e tratar o erro que virá da Store
+		// quando essa for cancelada.
 	}
 }
 
