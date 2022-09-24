11baafc92f2b33bb3184dc3ef8eb1f274d0c94df
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-23 23:36:19 -0300

    [feat] go com testes -- 20 Servidor HTTP -- TDD server implementado para passar o teste

--- Exercicios/GoComTestes/20ServidorHTTP/servidor.go
+++ Exercicios/GoComTestes/20ServidorHTTP/servidor.go
@@ -6,5 +6,16 @@ import (
 )
 
 func ServidorJogador(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	jogador := r.URL.Path[len("/jogadores/"):]
+
+	if jogador == "Leite" {
+		fmt.Fprint(w, "20")
+		return
+	}
+
+	if jogador == "Marcela" {
+		fmt.Fprint(w, "25")
+		return
+	}
+
 }
