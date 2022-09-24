97d95688e826a2c1a12bfc57e399148e1d805550
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-23 22:06:42 -0300

    [feat] go com testes -- 19 Context -- função refatorada para utilizar o contexto com downstream

--- Exercicios/GoComTestes/19Context/context.go
+++ Exercicios/GoComTestes/19Context/context.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"net/http"
 )
 
@@ -18,23 +18,10 @@ a goroutine e deixá-la processando não é nem eficiente nem ágil.
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		data := make(chan string, 1)
 
-		go func() { // fetch é executado e envia o resultado pelo canal
-			data <- store.Fetch() // com 1 buffer ou seja se chegar antes
-		}() // do ctx.Done é enviado se não apenas é cancelado.
-
-		select { // toma ação com o que chegar primeiro, o dado do fetch
-		case d := <-data: // ou o cancelamento do contexto.
-			fmt.Fprint(w, d)
-		case <-ctx.Done(): // sinal chamado quando o contexto finaliza.
-			store.Cancel()
-		}
 	}
 }
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
