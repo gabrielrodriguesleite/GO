a10eb4cfc071cd20af0e93e4d0e7b405a7b39e65
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-23 22:50:59 -0300

    [doc] go com testes -- 20 Servidor HTTP -- Desafio explicado

---
+++ Exercicios/GoComTestes/20ServidorHTTP/servidor_test.go
@@ -0,0 +1,9 @@
+package main
+
+// Desafio: Criar um servidor web para que os usuários
+// possam acompanhar quantas partidas os jogadores venceram.
+
+// GET /jogadores/{nome} deve retornar o numero total de vitórias
+
+// POST /jogadores/{nome} deve registrar uma vitória pra este nome de
+// jogador, incrementando a cada nova chamada de submissão de dados.
