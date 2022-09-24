32a8f0e189ae0f3506202b59713ff1522cccd771
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-23 22:54:53 -0300

    [doc] go com testes -- 20 Servidor HTTP -- Abordagem definida

--- Exercicios/GoComTestes/20ServidorHTTP/servidor_test.go
+++ Exercicios/GoComTestes/20ServidorHTTP/servidor_test.go
@@ -7,3 +7,14 @@ package main
 
 // POST /jogadores/{nome} deve registrar uma vitória pra este nome de
 // jogador, incrementando a cada nova chamada de submissão de dados.
+
+// ==================================================================
+
+// Abordagem: Desenvolvimento Orientado a Testes:
+// criar um software que funciona o mais rápido possível, e a cada ciclo
+// realizar pequenas melhorias até ter uma solução completa.
+
+// Essa abordagem tras algumas vantagems:
+// - Escopo de problema pequeno em todos os momentos.
+// - Poucos detalhes ajudam a manter o foco.
+// - Possibilidade de voltar o código em caso de bloqueio do desenvolvimento.
