3b45d108d982bed978e60b2e25bb1e1813f74196
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-23 23:45:27 -0300

    [test] go com testes -- 20 Servidor HTTP -- TDD teste refatorado

--- Exercicios/GoComTestes/20ServidorHTTP/servidor_test.go
+++ Exercicios/GoComTestes/20ServidorHTTP/servidor_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -56,30 +57,33 @@ import (
 
 func TestObterJogadores(t *testing.T) {
 	t.Run("retornar resultado de Leite", func(t *testing.T) {
-		requisicao, _ := http.NewRequest(http.MethodGet, "/jogadores/Leite", nil)
+		requisicao := novaRequisicaoObterPontuacao("Leite")
 		resposta := httptest.NewRecorder()
 
 		ServidorJogador(resposta, requisicao)
 
-		recebido := resposta.Body.String()
-		esperado := "20"
-
-		if recebido != esperado {
-			t.Errorf("recebido '%s', esperado '%s'", recebido, esperado)
-		}
+		verificaCorpoRequisicao(t, resposta.Body.String(), "20")
 	})
 
 	t.Run("retonrar resultado para Marcela", func(t *testing.T) {
-		requisicao, _ := http.NewRequest(http.MethodGet, "/jogadores/Marcela", nil)
+		requisicao := novaRequisicaoObterPontuacao("Marcela")
 		resposta := httptest.NewRecorder()
 
 		ServidorJogador(resposta, requisicao)
 
-		recebido := resposta.Body.String()
-		esperado := "25"
+		verificaCorpoRequisicao(t, resposta.Body.String(), "25")
 
-		if recebido != esperado {
-			t.Errorf("recebido '%s', esperado '%s'", recebido, esperado)
-		}
 	})
+}
+
+func novaRequisicaoObterPontuacao(nome string) *http.Request {
+	requisicao, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/jogadores/%s", nome), nil)
+	return requisicao
+}
+
+func verificaCorpoRequisicao(t *testing.T, recebido, esperado string) {
+	t.Helper()
+	if recebido != esperado {
+		t.Errorf("corpo da requisição é inválido, obtive '%s', esperava '%s'", recebido, esperado)
+	}
 }
