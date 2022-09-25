ef0f0e38d7e7d62e37c7b2534094b432e440fb43
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-24 23:19:46 -0300

    [feat] go com testes -- 20 Servidor HTTP -- TDD main implementando um armazenamento em memória

--- Exercicios/GoComTestes/20ServidorHTTP/main.go
+++ Exercicios/GoComTestes/20ServidorHTTP/main.go
@@ -9,9 +9,15 @@ func main() {
 	// tratador := http.HandlerFunc(ServidorJogador) // HandlerFunc é um adaptador
 	// que permite usar funções comuns como tratadores.
 	log.Println("Escutando na porta 5000")
-	servidor := &ServidorJogador{}
+	servidor := &ServidorJogador{&ArmazenamentoJogadorEmMemoria{}}
 	// if err := http.ListenAndServe(":5000", tratador); err != nil {
 	if err := http.ListenAndServe(":5000", servidor); err != nil {
 		log.Fatalf("não foi possível escutar na porta 5000 %v", err)
 	}
+}
+
+type ArmazenamentoJogadorEmMemoria struct{}
+
+func (a *ArmazenamentoJogadorEmMemoria) ObterPontuacaoJogador(nome string) int {
+	return 123
 }
