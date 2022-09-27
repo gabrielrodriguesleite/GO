f8e450c4f60547af8e348cd1f6bccfcb8e9d4ddf
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-26 22:21:01 -0300

    [ref] go com testes -- 20 Servidor HTTP -- TDD testes organizados antes das funções auxiliares

--- Exercicios/GoComTestes/20ServidorHTTP/servidor_test.go
+++ Exercicios/GoComTestes/20ServidorHTTP/servidor_test.go
@@ -99,6 +99,33 @@ func TestObterJogadores(t *testing.T) {
 	})
 }
 
+func TestArmazenamentoVitorias(t *testing.T) {
+	armazenamento := EsbocoArmazenamentoJogador{
+		map[string]int{},
+		[]string{},
+	}
+	servidor := &ServidorJogador{&armazenamento}
+
+	t.Run("registra vitorias na chamada ao método HTTP POST", func(t *testing.T) {
+		jogador := "Marcela"
+
+		requisicao := novaRequisicaoRegistrarVitoriaPost(jogador)
+		resposta := httptest.NewRecorder()
+
+		servidor.ServeHTTP(resposta, requisicao)
+
+		verificaRespostaCodigoStatus(t, resposta.Code, http.StatusAccepted)
+
+		if len(armazenamento.registrosVitorias) != 1 {
+			t.Errorf("verifiquei %d chamadas a RegistrarVitoria, esperava %d", len(armazenamento.registrosVitorias), 1)
+		}
+
+		if armazenamento.registrosVitorias[0] != jogador {
+			t.Errorf("não registrou o vencedor corretamente, recebi '%s', esperava '%s'", armazenamento.registrosVitorias[0], jogador)
+		}
+	})
+}
+
 func novaRequisicaoObterPontuacao(nome string) *http.Request {
 	requisicao, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/jogadores/%s", nome), nil)
 	return requisicao
@@ -130,33 +157,6 @@ func (e *EsbocoArmazenamentoJogador) ObterPontuacaoJogador(nome string) int {
 
 func (e *EsbocoArmazenamentoJogador) RegistrarVitoria(nome string) {
 	e.registrosVitorias = append(e.registrosVitorias, nome)
-}
-
-func TestArmazenamentoVitorias(t *testing.T) {
-	armazenamento := EsbocoArmazenamentoJogador{
-		map[string]int{},
-		[]string{},
-	}
-	servidor := &ServidorJogador{&armazenamento}
-
-	t.Run("registra vitorias na chamada ao método HTTP POST", func(t *testing.T) {
-		jogador := "Marcela"
-
-		requisicao := novaRequisicaoRegistrarVitoriaPost(jogador)
-		resposta := httptest.NewRecorder()
-
-		servidor.ServeHTTP(resposta, requisicao)
-
-		verificaRespostaCodigoStatus(t, resposta.Code, http.StatusAccepted)
-
-		if len(armazenamento.registrosVitorias) != 1 {
-			t.Errorf("verifiquei %d chamadas a RegistrarVitoria, esperava %d", len(armazenamento.registrosVitorias), 1)
-		}
-
-		if armazenamento.registrosVitorias[0] != jogador {
-			t.Errorf("não registrou o vencedor corretamente, recebi '%s', esperava '%s'", armazenamento.registrosVitorias[0], jogador)
-		}
-	})
 }
 
 func novaRequisicaoRegistrarVitoriaPost(nome string) *http.Request {
