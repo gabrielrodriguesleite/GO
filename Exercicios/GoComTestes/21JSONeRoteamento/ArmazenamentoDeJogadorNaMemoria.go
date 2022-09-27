b400d94ed60d427b83d5c71af8763211d8ac9909
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-26 22:47:32 -0300

    [feat] go com testes -- 21 JSON Roteamento e Aninhamento -- armazenamento em memoria copiado do exemplo anterior

---
+++ Exercicios/GoComTestes/21JSONeRoteamento/ArmazenamentoDeJogadorNaMemoria.go
@@ -0,0 +1,17 @@
+package main
+
+func NovoArmazenamentoJogadorEmMemoria() *ArmazenamentoJogadorEmMemoria {
+	return &ArmazenamentoJogadorEmMemoria{map[string]int{}}
+}
+
+type ArmazenamentoJogadorEmMemoria struct {
+	armazenamento map[string]int
+}
+
+func (a *ArmazenamentoJogadorEmMemoria) ObterPontuacaoJogador(nome string) int {
+	return a.armazenamento[nome]
+}
+
+func (a *ArmazenamentoJogadorEmMemoria) GravarVitoria(nome string) {
+	a.armazenamento[nome]++
+}
