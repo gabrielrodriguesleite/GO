9101a0b3278277090566104f8d869db1e5eb61be
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-26 22:34:28 -0300

    [feat] go com testes -- 20 Servidor HTTP -- TDD função novoArmazenamento... implementada métodos implementados

--- Exercicios/GoComTestes/20ServidorHTTP/main.go
+++ Exercicios/GoComTestes/20ServidorHTTP/main.go
@@ -9,17 +9,25 @@ func main() {
 	// tratador := http.HandlerFunc(ServidorJogador) // HandlerFunc é um adaptador
 	// que permite usar funções comuns como tratadores.
 	log.Println("Escutando na porta 5000")
-	servidor := &ServidorJogador{&ArmazenamentoJogadorEmMemoria{}}
+	servidor := &ServidorJogador{NovoArmazenamentoJogadorEmMemoria()}
 	// if err := http.ListenAndServe(":5000", tratador); err != nil {
 	if err := http.ListenAndServe(":5000", servidor); err != nil {
 		log.Fatalf("não foi possível escutar na porta 5000 %v", err)
 	}
 }
 
-type ArmazenamentoJogadorEmMemoria struct{}
-
-func (a *ArmazenamentoJogadorEmMemoria) ObterPontuacaoJogador(nome string) int {
-	return 123
+func NovoArmazenamentoJogadorEmMemoria() *ArmazenamentoJogadorEmMemoria {
+	return &ArmazenamentoJogadorEmMemoria{map[string]int{}}
 }
 
-func (a *ArmazenamentoJogadorEmMemoria) RegistrarVitoria(nome string) {}
+type ArmazenamentoJogadorEmMemoria struct {
+	armazenamento map[string]int
+}
+
+func (a *ArmazenamentoJogadorEmMemoria) ObterPontuacaoJogador(nome string) int {
+	return a.armazenamento[nome]
+}
+
+func (a *ArmazenamentoJogadorEmMemoria) RegistrarVitoria(nome string) {
+	a.armazenamento[nome]++
+}
