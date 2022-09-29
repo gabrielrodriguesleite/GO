1cc4db0ad4e2718f58685b836e476067d0c31e1b
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-28 21:36:51 -0300

    [feat] go com testes -- 22 IO e Sorting -- implementaç o do servidor reutilizado

---
+++ Exercicios/GoComTestes/22IOeSorting/servidor.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type ArmazenamentoJogador interface {
+	ObterPontuacaoJogador(nome string) int
+	GravarVitoria(nome string)
+	ObterLiga() []Jogador
+}
+
+type Jogador struct {
+	Nome     string
+	Vitorias int
+}
+
+type ServidorJogador struct {
+	armazenamento ArmazenamentoJogador
+	http.Handler  // incorporação do Handler dentro do ServidorJogador para acesso
+	// direto das funções.
+	// Mais em: https://golang.org/doc/effective_go.html#embedding
+}
+
+func NovoServidorJogador(armazenamento ArmazenamentoJogador) *ServidorJogador {
+	s := new(ServidorJogador)
+	s.armazenamento = armazenamento
+
+	roteador := http.NewServeMux()
+	roteador.Handle("/liga", http.HandlerFunc(s.manipulaLiga))
+	roteador.Handle("/jogadores/", http.HandlerFunc(s.manipulaJogadores))
+
+	s.Handler = roteador
+	return s
+}
+
+func (s *ServidorJogador) manipulaLiga(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	json.NewEncoder(w).Encode(s.armazenamento.ObterLiga())
+}
+
+func (s *ServidorJogador) manipulaJogadores(w http.ResponseWriter, r *http.Request) {
+	jogador := r.URL.Path[len("/jogadores/"):]
+
+	switch r.Method {
+	case http.MethodPost:
+		s.processarVitoria(w, jogador)
+	case http.MethodGet:
+		s.mostrarPontuacao(w, jogador)
+	}
+
+}
+
+func (s *ServidorJogador) mostrarPontuacao(w http.ResponseWriter, jogador string) {
+	pontuacao := s.armazenamento.ObterPontuacaoJogador(jogador)
+
+	if pontuacao == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, pontuacao)
+}
+
+func (s *ServidorJogador) processarVitoria(w http.ResponseWriter, jogador string) {
+	s.armazenamento.GravarVitoria(jogador)
+	w.WriteHeader(http.StatusAccepted)
+}
