531b2c182c4d31c31919fe3942d5dc89dfd67ef6
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-28 22:07:11 -0300

    [test] go com testes -- 22 IO e Sorting -- testes do novo sistema de armazenamento movido para o arquivo apropriado

--- Exercicios/GoComTestes/22IOeSorting/SistemaDeArquivoDeArmazenamentoDoJogador_test.go
+++ Exercicios/GoComTestes/22IOeSorting/SistemaDeArquivoDeArmazenamentoDoJogador_test.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSistemaDeArquivoDeArmazenamentoDoJogador(t *testing.T) {
+	t.Run("/liga de um leitor", func(t *testing.T) {
+
+		bancoDeDados := strings.NewReader(`[
+			{"Nome": "Leite", "Vitorias": 20},
+			{"Nome": "Marcela", "Vitorias" : 25}
+		]`)
+
+		armazenamento := SistemaDeArquivoDeArmazenamentoDoJogador{bancoDeDados}
+		recebido := armazenamento.PegaLiga()
+		esperado := []Jogador{
+			{"Leite", 20},
+			{"Marcela", 25},
+		}
+
+		defineLiga(t, recebido, esperado)
+	})
+}
