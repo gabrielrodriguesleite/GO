a7724d1dba4bd124d1ef59958d909ef163838bba
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-28 21:36:33 -0300

    [feat] go com testes -- 22 IO e Sorting -- implementaç o do Armazenamento em memória reutilizado

---
+++ Exercicios/GoComTestes/22IOeSorting/ArmazenamentoDeJogadorNaMemoria.go
@@ -0,0 +1,25 @@
+package main
+
+func NovoArmazenamentoJogadorEmMemoria() *ArmazenamentoJogadorEmMemoria {
+	return &ArmazenamentoJogadorEmMemoria{map[string]int{}}
+}
+
+type ArmazenamentoJogadorEmMemoria struct {
+	armazenamento map[string]int
+}
+
+func (a *ArmazenamentoJogadorEmMemoria) ObterPontuacaoJogador(nome string) int {
+	return a.armazenamento[nome]
+}
+
+func (a *ArmazenamentoJogadorEmMemoria) GravarVitoria(nome string) {
+	a.armazenamento[nome]++
+}
+
+func (a *ArmazenamentoJogadorEmMemoria) ObterLiga() []Jogador {
+	var liga []Jogador
+	for nome, vitorias := range a.armazenamento {
+		liga = append(liga, Jogador{nome, vitorias})
+	}
+	return liga
+}
