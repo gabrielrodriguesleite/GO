deec40633e9aeba6c684fcfe01a39455c744c675
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-28 22:09:02 -0300

    [feat] go com testes -- 22 IO e Sorting -- sistemadearquivo... implementado método PegaLiga

--- Exercicios/GoComTestes/22IOeSorting/SistemaDeArquivoDeArmazenamentoDoJogador.go
+++ Exercicios/GoComTestes/22IOeSorting/SistemaDeArquivoDeArmazenamentoDoJogador.go
@@ -1,3 +1,11 @@
 package main
 
-type SistemaDeArquivoDeArmazenamentoDoJogador struct{}
+import "io"
+
+type SistemaDeArquivoDeArmazenamentoDoJogador struct {
+	bancoDeDados io.Reader
+}
+
+func (s *SistemaDeArquivoDeArmazenamentoDoJogador) PegaLiga() []Jogador {
+	return nil
+}
