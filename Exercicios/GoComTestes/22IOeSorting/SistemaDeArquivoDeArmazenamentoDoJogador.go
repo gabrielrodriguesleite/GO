cb90e319050fbbb2ce9a99d3f96e007bd953137d
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-09-29 23:39:36 -0300

    [feat] go com testes -- 22 IO e Sorting -- DRY Sistema de arquivo de armazenamento incorpora ReadSeeker

--- Exercicios/GoComTestes/22IOeSorting/SistemaDeArquivoDeArmazenamentoDoJogador.go
+++ Exercicios/GoComTestes/22IOeSorting/SistemaDeArquivoDeArmazenamentoDoJogador.go
@@ -5,10 +5,11 @@ import (
 )
 
 type SistemaDeArquivoDeArmazenamentoDoJogador struct {
-	bancoDeDados io.Reader
+	bancoDeDados io.ReadSeeker
 }
 
 func (s *SistemaDeArquivoDeArmazenamentoDoJogador) PegaLiga() []Jogador {
+	s.bancoDeDados.Seek(0, 0)
 	liga, _ := NovaLiga(s.bancoDeDados)
 	return liga
 }
