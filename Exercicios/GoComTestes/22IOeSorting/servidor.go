5ee7e3bd637c9bdab6ed86a33f9bcae87d21d734
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-10-06 22:53:39 -0300

    [feat] go com testes -- 22 IO e Sorting -- ArmazenamentoJogador agora retorna Liga no método ObterLiga

--- Exercicios/GoComTestes/22IOeSorting/servidor.go
+++ Exercicios/GoComTestes/22IOeSorting/servidor.go
@@ -11,7 +11,7 @@ const tipoDoConteudoJSON = "application/json"
 type ArmazenamentoJogador interface {
 	ObterPontuacaoJogador(nome string) int
 	GravarVitoria(nome string)
-	ObterLiga() []Jogador
+	ObterLiga() Liga
 }
 
 type Jogador struct {
