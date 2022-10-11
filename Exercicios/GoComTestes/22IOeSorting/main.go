953e81b574e3b7ad8d7882a6216f912d48ac4b57
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-10-10 22:12:33 -0300

    [feat] go com testes -- 22 IO e Sorting -- main implementado usando armazenamento

--- Exercicios/GoComTestes/22IOeSorting/main.go
+++ Exercicios/GoComTestes/22IOeSorting/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("problema abrindo %s %v", dbFileName, err)
+	}
+
+	armazenamento := &SistemaDeArquivoDeArmazenamentoDoJogador{db}
 	log.Println("Escutando na porta 5000")
-	servidor := NovoServidorJogador(NovoArmazenamentoJogadorEmMemoria())
+	servidor := NovoServidorJogador(armazenamento)
+
 	if err := http.ListenAndServe(":5000", servidor); err != nil {
 		log.Fatalf("não foi possível escutar na porta 5000 %v", err)
 	}
@@ -17,3 +27,5 @@ func main() {
 // curl -X POST http://localhost:5000/jogadores/Leite
 // curl http://localhost:5000/jogadores/Leite
 // deve retornar 1
+
+// https://larien.gitbook.io/aprenda-go-com-testes/criando-uma-aplicacao/io#refatore
