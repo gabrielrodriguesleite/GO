ddf6e23a75d805d0f2266153e791871d05bdd40c
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-10-13 22:33:13 -0300

    [feat] go com testes -- 22 IO e Sorting -- função implementada para passar o caso de testes TestaFita_Escrita

--- Exercicios/GoComTestes/22IOeSorting/fita.go
+++ Exercicios/GoComTestes/22IOeSorting/fita.go
@@ -1,12 +1,15 @@
 package main
 
-import "io"
+import (
+	"os"
+)
 
 type fita struct {
-	arquivo io.ReadWriteSeeker
+	arquivo *os.File
 }
 
 func (f *fita) Write(p []byte) (n int, err error) {
+	f.arquivo.Truncate(0)
 	f.arquivo.Seek(0, 0)
 	return f.arquivo.Write(p)
 }
