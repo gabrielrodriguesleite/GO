9559c8b1a118b4f773aecdf3b6ac2fccc96642c8
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-10-13 22:26:40 -0300

    [test] go com testes -- 22 IO e Sorting -- caso de teste referente ao outro arquivo movido pra o arquivo apropriado

--- Exercicios/GoComTestes/22IOeSorting/servidor_test.go
+++ Exercicios/GoComTestes/22IOeSorting/servidor_test.go
@@ -4,10 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"reflect"
 	"testing"
 )
@@ -72,25 +70,6 @@ import (
 // depois criar uma implementação que dá suporte ao mecanismo preferido.]
 
 // ==================== TESTES PARTE 3 ====================
-
-func TestFita_Escrita(t *testing.T) {
-	arquivo, limpa := criaArquivoTemporario(t, "12345")
-	defer limpa()
-
-	fita := &fita{arquivo}
-	fita.Write([]byte("abc"))
-
-	arquivo.Seek(0, 0)
-	novoConteudoDoArquivo, _ := ioutil.ReadAll(arquivo)
-
-	recebido := string(novoConteudoDoArquivo)
-	esperado := "abc"
-
-	if recebido != esperado {
-		t.Errorf("recebido '%s' esperado '%s'", recebido, esperado)
-	}
-
-}
 
 func TestArmazenamentoDeSistemaDeArquivos(t *testing.T) {
 	t.Run("liga de um leitor", func(t *testing.T) {
@@ -392,21 +371,4 @@ func verificaTipoDoConteudo(t *testing.T, resposta *httptest.ResponseRecorder, e
 	if resposta.Result().Header.Get("content-type") != esperado {
 		t.Errorf("resposta não tinha o tipo de conteúdo de application/json, obtido %v", resposta.Result().Header)
 	}
-}
-
-func criaArquivoTemporario(t *testing.T, dadoInicial string) (io.ReadWriteSeeker, func()) {
-	t.Helper()
-	arquivotmp, err := ioutil.TempFile("", "db")
-	if err != nil {
-		t.Fatalf("não foi possível escrever o arquivo temporário %v", err)
-	}
-
-	arquivotmp.Write([]byte(dadoInicial))
-
-	removeArquivo := func() {
-		arquivotmp.Close()
-		os.Remove(arquivotmp.Name())
-	}
-
-	return arquivotmp, removeArquivo
 }
