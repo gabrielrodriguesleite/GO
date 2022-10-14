987245571cb536d789f140314036276b0ff0d2d5
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-10-13 22:33:52 -0300

    [doc] go com testes -- 22 IO e Sorting -- explicação das funções de arquivos utilizadas (truncate e seek)

--- Exercicios/GoComTestes/22IOeSorting/fita.go
+++ Exercicios/GoComTestes/22IOeSorting/fita.go
@@ -9,7 +9,7 @@ type fita struct {
 }
 
 func (f *fita) Write(p []byte) (n int, err error) {
-	f.arquivo.Truncate(0)
-	f.arquivo.Seek(0, 0)
+	f.arquivo.Truncate(0) // serve para esvaziar o aquivo
+	f.arquivo.Seek(0, 0)  // move o "cursor" para a posição de escrita definida
 	return f.arquivo.Write(p)
 }
